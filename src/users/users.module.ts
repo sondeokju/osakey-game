7678feb8301b7c8822df45aadc66b246db286cd0
sondeokju <thsejrwn78@naver.com>
2024-09-23 16:32:53 +0900

    log

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModel } from './entity/users.entity';
+import { AccountLevel } from 'src/static-table/account_level/entities/account_level.entity';
+import { AccountLevelService } from 'src/static-table/account_level/account_level.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersModel])],
+  imports: [TypeOrmModule.forFeature([UsersModel, AccountLevel])],
   exports: [UsersService],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AccountLevelService],
 })
 export class UsersModule {}
