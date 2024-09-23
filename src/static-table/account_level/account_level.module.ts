12bdabadbe27457001054d6637d0b5aa71eccaf4
sondeokju <thsejrwn78@naver.com>
2024-09-23 14:47:39 +0900

    log

--- src/static-table/account_level/account_level.module.ts
+++ src/static-table/account_level/account_level.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccountLevelService } from './account_level.service';
 import { AccountLevelController } from './account_level.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountLevel } from './entities/account_level.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AccountLevel])],
+  exports: [AccountLevelService],
   controllers: [AccountLevelController],
   providers: [AccountLevelService],
 })
--- src/static-table/reward_group/reward_group.module.ts
+++ src/static-table/reward_group/reward_group.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RewardGroupService } from './reward_group.service';
 import { RewardGroupController } from './reward_group.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RewardGroup } from './entities/reward_group.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RewardGroup])],
+  exports: [RewardGroupService],
   controllers: [RewardGroupController],
   providers: [RewardGroupService],
 })
