c0eb093efb5f39b5f2e9c185ed5f85f080fe8348
sondeokju <thsejrwn78@naver.com>
2024-09-26 15:05:43 +0900

    log

--- src/static-table/mission_sub/mission_sub.module.ts
+++ src/static-table/mission_sub/mission_sub.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MissionSubService } from './mission_sub.service';
 import { MissionSubController } from './mission_sub.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MissionSub } from './entities/mission_sub.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MissionSub])],
+  exports: [MissionSubService],
   controllers: [MissionSubController],
   providers: [MissionSubService],
 })
