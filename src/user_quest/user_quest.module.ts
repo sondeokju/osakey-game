40c8682836320bc3ac2dcd8397c08751ef7fa28a
sondeokju <thsejrwn78@naver.com>
2024-09-30 17:56:29 +0900

    log

--- src/user_quest/user_quest.module.ts
+++ src/user_quest/user_quest.module.ts
@@ -7,11 +7,20 @@ import { RewardGroupService } from 'src/static-table/reward_group/reward_group.s
 import { MissionRoutineService } from 'src/static-table/mission_routine/mission_routine.service';
 import { RewardGroup } from 'src/static-table/reward_group/entities/reward_group.entity';
 import { MissionRoutine } from 'src/static-table/mission_routine/entities/mission_routine.entity';
+import { ItemService } from 'src/static-table/item/item.service';
+import { Item } from 'src/static-table/item/entities/item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserQuest, RewardGroup, MissionRoutine])],
+  imports: [
+    TypeOrmModule.forFeature([UserQuest, RewardGroup, MissionRoutine, Item]),
+  ],
   exports: [UserQuestService],
   controllers: [UserQuestController],
-  providers: [UserQuestService, RewardGroupService, MissionRoutineService],
+  providers: [
+    UserQuestService,
+    RewardGroupService,
+    MissionRoutineService,
+    ItemService,
+  ],
 })
 export class UserQuestModule {}
