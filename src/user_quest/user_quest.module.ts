e1e5d15061046e1f62e2742abfade7f5842df1f4
sondeokju <thsejrwn78@naver.com>
2024-09-30 17:42:56 +0900

    log

--- src/user_quest/user_quest.module.ts
+++ src/user_quest/user_quest.module.ts
@@ -3,11 +3,13 @@ import { UserQuestService } from './user_quest.service';
 import { UserQuestController } from './user_quest.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserQuest } from './entity/user_quest.entity';
+import { RewardGroupService } from 'src/static-table/reward_group/reward_group.service';
+import { MissionRoutineService } from 'src/static-table/mission_routine/mission_routine.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserQuest])],
   exports: [UserQuestService],
   controllers: [UserQuestController],
-  providers: [UserQuestService],
+  providers: [UserQuestService, RewardGroupService, MissionRoutineService],
 })
 export class UserQuestModule {}
