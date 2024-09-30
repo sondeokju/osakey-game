e5ae840935f0c92ae15de2952d0a42be52b6d802
sondeokju <thsejrwn78@naver.com>
2024-09-30 17:38:40 +0900

    log

--- src/static-table/mission_routine/mission_routine.module.ts
+++ src/static-table/mission_routine/mission_routine.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MissionRoutineService } from './mission_routine.service';
 import { MissionRoutineController } from './mission_routine.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MissionRoutine } from './entities/mission_routine.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MissionRoutine])],
+  exports: [MissionRoutineService],
   controllers: [MissionRoutineController],
   providers: [MissionRoutineService],
 })
