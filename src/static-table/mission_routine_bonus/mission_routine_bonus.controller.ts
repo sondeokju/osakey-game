bbeceac702da9716a913780cd17995f25cb94823
sondeokju <thsejrwn78@naver.com>
2024-10-04 11:59:47 +0900

    log

--- src/static-table/mission_routine_bonus/mission_routine_bonus.controller.ts
+++ src/static-table/mission_routine_bonus/mission_routine_bonus.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { MissionRoutineBonusService } from './mission_routine_bonus.service';
 import { CreateMissionRoutineBonusDto } from './dto/create-mission_routine_bonus.dto';
 import { UpdateMissionRoutineBonusDto } from './dto/update-mission_routine_bonus.dto';
 
 @Controller('mission-routine-bonus')
 export class MissionRoutineBonusController {
-  constructor(private readonly missionRoutineBonusService: MissionRoutineBonusService) {}
-
-  @Post()
-  create(@Body() createMissionRoutineBonusDto: CreateMissionRoutineBonusDto) {
-    return this.missionRoutineBonusService.create(createMissionRoutineBonusDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.missionRoutineBonusService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.missionRoutineBonusService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMissionRoutineBonusDto: UpdateMissionRoutineBonusDto) {
-    return this.missionRoutineBonusService.update(+id, updateMissionRoutineBonusDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.missionRoutineBonusService.remove(+id);
-  }
+  constructor(
+    private readonly missionRoutineBonusService: MissionRoutineBonusService,
+  ) {}
 }
