43efdf8903d3d81ba76872eea53766de39e30127
sondeokju <thsejrwn78@naver.com>
2024-10-08 17:01:09 +0900

    log

--- src/static-table/hero/hero.module.ts
+++ src/static-table/hero/hero.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HeroService } from './hero.service';
 import { HeroController } from './hero.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hero } from './entities/hero.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Hero])],
+  exports: [HeroService],
   controllers: [HeroController],
   providers: [HeroService],
 })
