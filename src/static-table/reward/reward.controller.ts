8e6ae2ed7aebce50007e9a2f8433f117c40a1f54
sondeokju <thsejrwn78@naver.com>
2024-10-08 11:12:35 +0900

    log

--- src/static-table/reward/reward.controller.ts
+++ src/static-table/reward/reward.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RewardService } from './reward.service';
 import { CreateRewardDto } from './dto/create-reward.dto';
 import { UpdateRewardDto } from './dto/update-reward.dto';
@@ -6,29 +14,4 @@ import { UpdateRewardDto } from './dto/update-reward.dto';
 @Controller('reward')
 export class RewardController {
   constructor(private readonly rewardService: RewardService) {}
-
-  @Post()
-  create(@Body() createRewardDto: CreateRewardDto) {
-    return this.rewardService.create(createRewardDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.rewardService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rewardService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRewardDto: UpdateRewardDto) {
-    return this.rewardService.update(+id, updateRewardDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rewardService.remove(+id);
-  }
 }
