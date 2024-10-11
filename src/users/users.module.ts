4a2670b883b4da5eccc71096f3c8f36716dbda24
sondeokju <thsejrwn78@naver.com>
2024-10-11 17:43:47 +0900

    log

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,7 +8,6 @@ import { HeroService } from 'src/static-table/hero/hero.service';
 import { Item } from 'src/static-table/item/entities/item.entity';
 import { ItemService } from 'src/static-table/item/item.service';
 
-@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([Users, Hero, Item])],
   exports: [UsersService],
