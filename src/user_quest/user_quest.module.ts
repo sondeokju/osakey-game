2fc0737c22515757065f10a988e797ded0be9c1e
sondeokju <thsejrwn78@naver.com>
2024-10-22 14:41:23 +0900

    log

--- src/user_quest/user_quest.module.ts
+++ src/user_quest/user_quest.module.ts
@@ -25,6 +25,7 @@ import { Users } from 'src/users/entity/users.entity';
 import { UsersService } from 'src/users/users.service';
 import { Hero } from 'src/static-table/hero/entities/hero.entity';
 import { HeroService } from 'src/static-table/hero/hero.service';
+import { RewardInvenService } from 'src/super/reward_inven/reward_inven.service';
 
 @Module({
   imports: [
@@ -60,6 +61,7 @@ import { HeroService } from 'src/static-table/hero/hero.service';
     UsersService,
     HeroService,
     RewardService,
+    RewardInvenService,
   ],
 })
 export class UserQuestModule {}
