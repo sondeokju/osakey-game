1b06205c030316273ec628a7a053dc7b698e9fc7
sondeokju <thsejrwn78@naver.com>
2024-10-23 10:54:03 +0900

    log

--- src/users/entity/users.entity.ts
+++ src/users/entity/users.entity.ts
@@ -49,7 +49,7 @@ export class Users extends BaseModel {
   @IsString({
     message: stringValidatioMessage,
   })
-  @Index({ unique: true })
+  @Index({ unique: false })
   device_id: string;
 
   @Column({
@@ -59,7 +59,7 @@ export class Users extends BaseModel {
   @IsString({
     message: stringValidatioMessage,
   })
-  @Index({ unique: true })
+  @Index({ unique: false })
   pgs_id: string;
 
   @Column({
