4f457d6b174399530790e4b01709520bcf51f2e1
sondeokju <thsejrwn78@naver.com>
2024-10-28 19:15:33 +0900

    log

--- src/app.module.ts
+++ src/app.module.ts
@@ -81,9 +81,12 @@ import { RewardInvenModule } from './super/reward_inven/reward_inven.module';
 import { LogUrlModule } from './common/log_url/log_url.module';
 import { LogUrl } from './common/log_url/entities/log_url.entity';
 import { RequestLoggingMiddleware } from './common/middleware/request-logging.middleware';
+import { statusMonitorConfig } from './common/monitor/status-monitor.config';
+import { StatusMonitorModule } from 'nestjs-status-monitor';
 
 @Module({
   imports: [
+    StatusMonitorModule.forRoot(statusMonitorConfig),
     ServeStaticModule.forRoot({
       rootPath: PUBLIC_FOLDER_PATH,
       serveRoot: '/public',
