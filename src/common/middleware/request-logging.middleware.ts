bed33b2b07a9c9a043e9c8c3ebb0d86a7aa866e8
sondeokju <thsejrwn78@naver.com>
2024-10-28 14:48:11 +0900

    log

--- src/common/middleware/request-logging.middleware.ts
+++ src/common/middleware/request-logging.middleware.ts
@@ -1,13 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Request, Response, NextFunction } from 'express';
-import { LogUrl } from '../log_url/entities/log_url.entity';
-import { Repository } from 'typeorm';
 
-// @Injectable()
-// export class RequestLoggingMiddleware implements NestMiddleware {
-//   //   constructor(private readonly logUrlRepository: LogUrl) {}
-//   //   async use(req: Request, res: Response, next: NextFunction) {
-//   //     next();
-//   //   }
-// }
+@Injectable()
+export class RequestLoggingMiddleware implements NestMiddleware {
+  //constructor(private readonly logUrlRepository: LogUrl) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    next();
+  }
+}
