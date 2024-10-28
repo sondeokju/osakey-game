60919ac1811c303207236c6f3c6f2d65cd15a60a
sondeokju <thsejrwn78@naver.com>
2024-10-28 14:28:48 +0900

    log

--- src/common/log_url/log_url.module.ts
+++ src/common/log_url/log_url.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LogUrlService } from './log_url.service';
 import { LogUrlController } from './log_url.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LogUrl } from './entities/log_url.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LogUrl])],
+  exports: [LogUrlService],
   controllers: [LogUrlController],
   providers: [LogUrlService],
 })
