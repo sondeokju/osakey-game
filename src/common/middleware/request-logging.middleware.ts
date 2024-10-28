8be3f6f713a1d1f0adcb1249ad772a779f28f01a
sondeokju <thsejrwn78@naver.com>
2024-10-28 20:23:36 +0900

    log

--- src/common/middleware/request-logging.middleware.ts
+++ src/common/middleware/request-logging.middleware.ts
@@ -7,11 +7,13 @@ export class RequestLoggingMiddleware implements NestMiddleware {
   constructor(private readonly logUrlService: LogUrlService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const url = req.baseUrl;
+    //const url = req.baseUrl;
+    const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+    console.log(fullUrl); // 전체 URL 확인용
     const method = req.method;
     const create_at = new Date();
 
-    await this.logUrlService.urlLog(url, method, create_at);
+    await this.logUrlService.urlLog(fullUrl, method, create_at);
     next();
   }
 }
