e0e2fe989c9e44221ba9ceea60494400267053d1
sondeokju <thsejrwn78@naver.com>
2024-10-28 14:46:48 +0900

    log

--- src/common/middleware/request-logging.middleware.ts
+++ src/common/middleware/request-logging.middleware.ts
@@ -4,17 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 import { LogUrl } from '../log_url/entities/log_url.entity';
 import { Repository } from 'typeorm';
 
-@Injectable()
-export class RequestLoggingMiddleware implements NestMiddleware {
-  constructor(private readonly logUrlRepository: LogUrl) {}
-
-  async use(req: Request, res: Response, next: NextFunction) {
-    // const log = this.logUrlRepository.create({
-    //   url: req.url,
-    //   method: req.method,
-    //   created_at: new Date(),
-    // });
-    // await this.logUrlRepository.save(log);
-    next();
-  }
-}
+// @Injectable()
+// export class RequestLoggingMiddleware implements NestMiddleware {
+//   //   constructor(private readonly logUrlRepository: LogUrl) {}
+//   //   async use(req: Request, res: Response, next: NextFunction) {
+//   //     next();
+//   //   }
+// }
