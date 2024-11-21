10e6f2f4f292e2c7327ca4c14bac85d4b62b1e5f
sondeokju <thsejrwn78@naver.com>
2024-11-21 18:16:48 +0900

    log

--- src/inventory/sns/user_sns_reward/user_sns_reward.module.ts
+++ src/inventory/sns/user_sns_reward/user_sns_reward.module.ts
@@ -12,10 +12,19 @@ import { UsersService } from 'src/users/users.service';
 import { Users } from 'src/users/entity/users.entity';
 import { ItemService } from 'src/static-table/item/item.service';
 import { Item } from 'src/static-table/item/entities/item.entity';
+import { UserItemService } from 'src/user_item/user_item.service';
+import { UserItem } from 'src/user_item/entities/user_item.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserSnsReward, SnsReward, Reward, Users, Item]),
+    TypeOrmModule.forFeature([
+      UserSnsReward,
+      SnsReward,
+      Reward,
+      Users,
+      Item,
+      UserItem,
+    ]),
   ],
   exports: [UserSnsRewardService],
   controllers: [UserSnsRewardController],
@@ -26,6 +35,7 @@ import { Item } from 'src/static-table/item/entities/item.entity';
     RewardService,
     UsersService,
     ItemService,
+    UserItemService,
   ],
 })
 export class UserSnsRewardModule {}
