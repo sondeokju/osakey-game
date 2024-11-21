4736479914194d6c063098999e7e16ea3e16c619
sondeokju <thsejrwn78@naver.com>
2024-11-21 18:13:59 +0900

    log

--- src/inventory/sns/user_sns_reward/user_sns_reward.module.ts
+++ src/inventory/sns/user_sns_reward/user_sns_reward.module.ts
@@ -6,11 +6,18 @@ import { UserSnsReward } from './entities/user_sns_reward.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RewardOfferService } from 'src/supervisor/reward_offer/reward_offer.service';
 import { SnsRewardService } from 'src/static-table/sns/sns_reward/sns_reward.service';
+import { RewardService } from 'src/static-table/reward/reward.service';
+import { Reward } from 'src/static-table/reward/entities/reward.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserSnsReward, SnsReward])],
+  imports: [TypeOrmModule.forFeature([UserSnsReward, SnsReward, Reward])],
   exports: [UserSnsRewardService],
   controllers: [UserSnsRewardController],
-  providers: [UserSnsRewardService, SnsRewardService, RewardOfferService],
+  providers: [
+    UserSnsRewardService,
+    SnsRewardService,
+    RewardOfferService,
+    RewardService,
+  ],
 })
 export class UserSnsRewardModule {}
