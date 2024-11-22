f15c0f04af938bec792bc8ee3f66e425c5c36691
sondeokju <thsejrwn78@naver.com>
2024-11-22 19:08:41 +0900

    log

--- src/inventory/sns/user_sns_level/user_sns_level.module.ts
+++ src/inventory/sns/user_sns_level/user_sns_level.module.ts
@@ -10,14 +10,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserTunaTvService } from '../user_tuna_tv/user_tuna_tv.service';
 import { UserTunaTv } from '../user_tuna_tv/entities/user_tuna_tv.entity';
 import { RewardOfferService } from 'src/supervisor/reward_offer/reward_offer.service';
-// import { Hero } from 'src/static-table/hero/entities/hero.entity';
-// import { HeroService } from 'src/static-table/hero/hero.service';
+import { Hero } from 'src/static-table/hero/entities/hero.entity';
+import { HeroService } from 'src/static-table/hero/hero.service';
 import { Item } from 'src/static-table/item/entities/item.entity';
 import { ItemService } from 'src/static-table/item/item.service';
 import { Reward } from 'src/static-table/reward/entities/reward.entity';
 import { RewardService } from 'src/static-table/reward/reward.service';
-import { UserItem } from 'src/user_item/entities/user_item.entity';
-import { UserItemService } from 'src/user_item/user_item.service';
+// import { UserItem } from 'src/user_item/entities/user_item.entity';
+// import { UserItemService } from 'src/user_item/user_item.service';
 import { Users } from 'src/users/entity/users.entity';
 import { UsersService } from 'src/users/users.service';
 
@@ -28,8 +28,8 @@ import { UsersService } from 'src/users/users.service';
       Reward,
       Users,
       Item,
-      UserItem,
-      //Hero,
+      //UserItem,
+      Hero,
       UserTunaTv,
       SnsLevel,
       SnsReward,
@@ -43,8 +43,8 @@ import { UsersService } from 'src/users/users.service';
     RewardService,
     UsersService,
     ItemService,
-    UserItemService,
-    //HeroService,
+    //UserItemService,
+    HeroService,
     UserTunaTvService,
     SnsLevelService,
     SnsRewardService,
