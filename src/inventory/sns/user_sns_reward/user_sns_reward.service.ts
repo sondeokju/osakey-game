0e479876d880d2d36219d3f3fac29bc98b6d8e5b
sondeokju <thsejrwn78@naver.com>
2024-11-22 18:57:26 +0900

    log

--- src/inventory/sns/user_sns_reward/user_sns_reward.service.ts
+++ src/inventory/sns/user_sns_reward/user_sns_reward.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryRunner, Repository } from 'typeorm';
-import { SnsRewardService } from 'src/static-table/sns/sns_reward/sns_reward.service';
 import { UserSnsReward } from './entities/user_sns_reward.entity';
-import { RewardOfferService } from 'src/supervisor/reward_offer/reward_offer.service';
 
 @Injectable()
 export class UserSnsRewardService {
   constructor(
     @InjectRepository(UserSnsReward)
     private readonly userSnsRewardRepository: Repository<UserSnsReward>,
-    private readonly snsRewardService: SnsRewardService,
-    private readonly rewardOfferService: RewardOfferService,
   ) {}
 
   getSnsRewardRepository(qr?: QueryRunner) {
