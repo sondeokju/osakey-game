e7411c3869fd6f7f142d01a543d2774df9dc7f8d
sondeokju <thsejrwn78@naver.com>
2024-11-22 17:04:12 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.module.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserTunaTvOnlineService } from './user-tuna-tv-online.service';
 import { UserTunaTvOnlineController } from './user-tuna-tv-online.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserTunaTvOnline } from './entities/user-tuna-tv-online.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserTunaTvOnline])],
+  exports: [UserTunaTvOnlineService],
   controllers: [UserTunaTvOnlineController],
   providers: [UserTunaTvOnlineService],
 })
