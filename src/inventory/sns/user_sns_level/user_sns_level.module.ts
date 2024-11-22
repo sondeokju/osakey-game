016e7f360c5dfb904636a9d46150d8d06ebceadb
sondeokju <thsejrwn78@naver.com>
2024-11-22 18:49:26 +0900

    log

--- src/inventory/sns/user_sns_level/user_sns_level.module.ts
+++ src/inventory/sns/user_sns_level/user_sns_level.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserSnsLevelService } from './user_sns_level.service';
 import { UserSnsLevelController } from './user_sns_level.controller';
-import { SnsConfig } from 'src/static-table/sns/sns_config/entities/sns_config.entity';
 import { SnsLevel } from 'src/static-table/sns/sns_level/entities/sns_level.entity';
-import { SnsLikeRule } from 'src/static-table/sns/sns_like_rule/entities/sns_like_rule.entity';
 import { SnsReward } from 'src/static-table/sns/sns_reward/entities/sns_reward.entity';
-import { SnsConfigService } from 'src/static-table/sns/sns_config/sns_config.service';
 import { SnsLevelService } from 'src/static-table/sns/sns_level/sns_level.service';
-import { SnsLikeRuleService } from 'src/static-table/sns/sns_like_rule/sns_like_rule.service';
 import { SnsRewardService } from 'src/static-table/sns/sns_reward/sns_reward.service';
 import { UserSnsLevel } from './entities/user_sns_level.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -29,35 +25,29 @@ import { UsersService } from 'src/users/users.service';
   imports: [
     TypeOrmModule.forFeature([
       UserSnsLevel,
-      SnsConfig,
-      SnsLevel,
-      SnsLikeRule,
       SnsReward,
-      UserTunaTv,
       Reward,
       Users,
-      //Item,
-      //UserItem,
-      //Hero,
+      Item,
+      UserItem,
+      Hero,
+      UserTunaTv,
+      SnsLevel,
     ]),
   ],
   exports: [UserSnsLevelService],
   controllers: [UserSnsLevelController],
   providers: [
     UserSnsLevelService,
-    SnsConfigService,
-    SnsLevelService,
-    SnsLikeRuleService,
-    SnsRewardService,
-    UserTunaTvService,
     SnsRewardService,
     RewardOfferService,
     RewardService,
     UsersService,
-    //ItemService,
-    //UserItemService,
-    //HeroService,
-    ,
+    ItemService,
+    UserItemService,
+    HeroService,
+    UserTunaTvService,
+    SnsLevelService,
   ],
 })
 export class UserSnsLevelModule {}
