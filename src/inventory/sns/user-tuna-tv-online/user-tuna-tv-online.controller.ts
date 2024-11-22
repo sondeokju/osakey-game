4f391f2213ba570837cc98ff515f0d12e5cd7844
sondeokju <thsejrwn78@naver.com>
2024-11-22 17:01:15 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.controller.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 import { UserTunaTvOnlineService } from './user-tuna-tv-online.service';
+import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { User } from 'src/users/decorator/user.decorator';
+import { QueryRunner } from 'src/common/decorator/query-runner.decorator';
+import { QueryRunner as QR } from 'typeorm';
+import { Users } from 'src/users/entity/users.entity';
 
-@Controller('user-tuna-tv-online')
+@Controller('tuna-tv-online')
 export class UserTunaTvOnlineController {
   constructor(
     private readonly userTunaTvOnlineService: UserTunaTvOnlineService,
   ) {}
+
+  @Get('data')
+  @UseInterceptors(TransactionInterceptor)
+  async FollowList(@User() user: Users, @QueryRunner() qr: QR) {
+    const result = this.userTunaTvOnlineService.tunaTvOnlineList(qr);
+    return JSON.stringify(result);
+  }
 }
--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QueryRunner, Repository } from 'typeorm';
+import { UserTunaTvOnline } from './entities/user-tuna-tv-online.entity';
 
 @Injectable()
-export class UserTunaTvOnlineService {}
+export class UserTunaTvOnlineService {
+  constructor(
+    @InjectRepository(UserTunaTvOnline)
+    private readonly userTunaTvOnlineRepository: Repository<UserTunaTvOnline>,
+  ) {}
+
+  getUserTunaTvOnlineRepository(qr?: QueryRunner) {
+    return qr
+      ? qr.manager.getRepository<UserTunaTvOnline>(UserTunaTvOnline)
+      : this.userTunaTvOnlineRepository;
+  }
+
+  async tunaTvOnlineList(qr?: QueryRunner) {
+    const userTunaTvOnlineRepository = this.getUserTunaTvOnlineRepository(qr);
+    const userTunaTvOnlineData = await userTunaTvOnlineRepository.find();
+
+    return userTunaTvOnlineData;
+  }
+}
