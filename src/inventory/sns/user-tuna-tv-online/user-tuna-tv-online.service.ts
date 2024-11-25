9b3440f6e2fcaaad587b3bab6f8b4a65e19e86e8
sondeokju <thsejrwn78@naver.com>
2024-11-25 13:43:49 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
@@ -21,16 +21,19 @@ export class UserTunaTvOnlineService {
     const userTunaTvOnlineRepository = this.getUserTunaTvOnlineRepository(qr);
     //const userTunaTvOnlineData = await userTunaTvOnlineRepository.find();
     const result = await userTunaTvOnlineRepository
-      .createQueryBuilder()
-      .select('user_tuna_tv_online.tuna_tv_id', 'user_tuna_tv.ingame_kind')
-      .from(UserTunaTv, 'user_tuna_tv')
-      .addSelect('user_tuna_tv.select_1', 'user_tuna_tv.select_2')
-      .addSelect('user_tuna_tv.select_3')
+      .createQueryBuilder('user_tuna_tv_online') // 기본 테이블 별칭 설정
+      .select('user_tuna_tv_online.tuna_tv_id', 'tunaTvId') // 첫 번째 컬럼 선택
+      .addSelect('user_tuna_tv.ingame_kind', 'ingameKind') // 조인된 테이블 컬럼 선택
+      .addSelect([
+        'user_tuna_tv.select_1',
+        'user_tuna_tv.select_2',
+        'user_tuna_tv.select_3',
+      ]) // 추가 컬럼 선택
       .innerJoin(
-        UserTunaTvOnline,
-        'user_tuna_tv_online',
-        'user_tuna_tv_online.tuna_tv_id = tuna_tv_id',
-      ) // 조인 조건
+        UserTunaTv, // 조인 대상 테이블
+        'user_tuna_tv', // 조인 대상 테이블의 별칭
+        'user_tuna_tv_online.tuna_tv_id = user_tuna_tv.tuna_tv_id', // 조인 조건
+      )
       .getRawMany(); // Raw 데이터 가져오기
 
     return result;
