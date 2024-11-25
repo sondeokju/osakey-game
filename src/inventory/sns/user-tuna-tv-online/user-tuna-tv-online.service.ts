cd67b86f8f1954a2eaee2afc945fc7bf404c9069
sondeokju <thsejrwn78@naver.com>
2024-11-25 12:24:27 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryRunner, Repository } from 'typeorm';
 import { UserTunaTvOnline } from './entities/user-tuna-tv-online.entity';
+import { UserTunaTv } from '../user_tuna_tv/entities/user_tuna_tv.entity';
 
 @Injectable()
 export class UserTunaTvOnlineService {
@@ -18,8 +19,24 @@ export class UserTunaTvOnlineService {
 
   async tunaTvOnlineList(qr?: QueryRunner) {
     const userTunaTvOnlineRepository = this.getUserTunaTvOnlineRepository(qr);
-    const userTunaTvOnlineData = await userTunaTvOnlineRepository.find();
+    //const userTunaTvOnlineData = await userTunaTvOnlineRepository.find();
+    const result = await userTunaTvOnlineRepository
+      .createQueryBuilder()
+      .select('user_tuna_tv_online.tuna_tv_id')
+      .addSelect(
+        'user_tuna_tv.ingame_kind',
+        'user_tuna_tv.select_1',
+        'user_tuna_tv.select_2',
+        'user_tuna_tv.select_3',
+      )
+      .from(UserTunaTv, 'user_tuna_tv')
+      .innerJoin(
+        UserTunaTvOnline,
+        'user_tuna_tv_online',
+        'user_tuna_tv_online.tuna_tv_id = tuna_tv_id',
+      ) // 조인 조건
+      .getRawMany(); // Raw 데이터 가져오기
 
-    return userTunaTvOnlineData;
+    return result;
   }
 }
--- src/inventory/sns/user_tuna_tv/user_tuna_tv.service.ts
+++ src/inventory/sns/user_tuna_tv/user_tuna_tv.service.ts
@@ -2,10 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserTunaTv } from './entities/user_tuna_tv.entity';
 import { QueryRunner, Repository } from 'typeorm';
-// import { SnsLevelService } from 'src/static-table/sns/sns_level/sns_level.service';
-// import { SnsConfigService } from 'src/static-table/sns/sns_config/sns_config.service';
-// import { SnsLikeRuleService } from 'src/static-table/sns/sns_like_rule/sns_like_rule.service';
-// import { SnsRewardService } from 'src/static-table/sns/sns_reward/sns_reward.service';
 
 @Injectable()
 export class UserTunaTvService {
