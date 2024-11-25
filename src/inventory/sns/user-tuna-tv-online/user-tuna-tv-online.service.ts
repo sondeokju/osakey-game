fe29aab140f58642f0f71b63feace1f0d316e5ad
sondeokju <thsejrwn78@naver.com>
2024-11-25 14:05:09 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { QueryRunner, Repository } from 'typeorm';
+import { QueryRunner, Repository, DataSource } from 'typeorm';
 import { UserTunaTvOnline } from './entities/user-tuna-tv-online.entity';
-import { UserTunaTv } from '../user_tuna_tv/entities/user_tuna_tv.entity';
 
 @Injectable()
 export class UserTunaTvOnlineService {
   constructor(
     @InjectRepository(UserTunaTvOnline)
     private readonly userTunaTvOnlineRepository: Repository<UserTunaTvOnline>,
+    private dataSource: DataSource,
   ) {}
 
   getUserTunaTvOnlineRepository(qr?: QueryRunner) {
@@ -17,23 +17,41 @@ export class UserTunaTvOnlineService {
       : this.userTunaTvOnlineRepository;
   }
 
-  async tunaTvOnlineList(qr?: QueryRunner) {
-    const userTunaTvOnlineRepository = this.getUserTunaTvOnlineRepository(qr);
-    const result = await userTunaTvOnlineRepository
-      .createQueryBuilder('a') // `user_tuna_tv_online` 테이블의 별칭을 `a`로 설정
-      .select('a.tuna_tv_id', 'tunaTvId') // 첫 번째 테이블에서 tuna_tv_id 선택
-      .addSelect('b.ingame_kind', 'ingameKind') // 두 번째 테이블에서 ingame_kind 선택
-      .addSelect('b.select_1', 'select1')
-      .addSelect('b.select_2', 'select2')
-      .addSelect('b.select_3', 'select3')
-      .addSelect('b.score', 'score') // 추가 선택
-      .innerJoin(
-        'user_tuna_tv', // 조인 대상 테이블
-        'b', // 조인 대상 테이블의 별칭
-        'a.tuna_tv_id = b.id', // 조인 조건
-      )
-      .getRawMany(); // Raw 데이터 가져오기
+  // async tunaTvOnlineList(qr?: QueryRunner) {
+  //   const userTunaTvOnlineRepository = this.getUserTunaTvOnlineRepository(qr);
+  //   const result = await userTunaTvOnlineRepository
+  //     .createQueryBuilder('a') // `user_tuna_tv_online` 테이블의 별칭을 `a`로 설정
+  //     .select('a.tuna_tv_id', 'tunaTvId') // 첫 번째 테이블에서 tuna_tv_id 선택
+  //     .addSelect('b.ingame_kind', 'ingameKind') // 두 번째 테이블에서 ingame_kind 선택
+  //     .addSelect('b.select_1', 'select1')
+  //     .addSelect('b.select_2', 'select2')
+  //     .addSelect('b.select_3', 'select3')
+  //     .addSelect('b.score', 'score') // 추가 선택
+  //     .innerJoin(
+  //       'user_tuna_tv', // 조인 대상 테이블
+  //       'b', // 조인 대상 테이블의 별칭
+  //       'a.tuna_tv_id = b.id', // 조인 조건
+  //     )
+  //     .getRawMany(); // Raw 데이터 가져오기
+
+  //   return result;
+  // }
+
+  async tunaTvOnlineList() {
+    const result = await this.executeRawQuery();
 
     return result;
+  }
+
+  async executeRawQuery() {
+    const query = `
+              SELECT a.tuna_tv_id, b.ingame_kind, b.select_1, b.select_2, b.select_3, b.score
+              FROM user_tuna_tv_online a
+              inner join user_tuna_tv b
+              on a.tuna_tv_id = b.id;`;
+
+    console.log(query);
+
+    return await this.dataSource.query(query);
   }
 }
