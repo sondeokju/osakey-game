f61a17c5b29e20b4e702956cdeb5d2868afd0b18
sondeokju <thsejrwn78@naver.com>
2024-11-25 14:08:00 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
@@ -17,31 +17,31 @@ export class UserTunaTvOnlineService {
       : this.userTunaTvOnlineRepository;
   }
 
-  // async tunaTvOnlineList(qr?: QueryRunner) {
-  //   const userTunaTvOnlineRepository = this.getUserTunaTvOnlineRepository(qr);
-  //   const result = await userTunaTvOnlineRepository
-  //     .createQueryBuilder('a') // `user_tuna_tv_online` 테이블의 별칭을 `a`로 설정
-  //     .select('a.tuna_tv_id', 'tunaTvId') // 첫 번째 테이블에서 tuna_tv_id 선택
-  //     .addSelect('b.ingame_kind', 'ingameKind') // 두 번째 테이블에서 ingame_kind 선택
-  //     .addSelect('b.select_1', 'select1')
-  //     .addSelect('b.select_2', 'select2')
-  //     .addSelect('b.select_3', 'select3')
-  //     .addSelect('b.score', 'score') // 추가 선택
-  //     .innerJoin(
-  //       'user_tuna_tv', // 조인 대상 테이블
-  //       'b', // 조인 대상 테이블의 별칭
-  //       'a.tuna_tv_id = b.id', // 조인 조건
-  //     )
-  //     .getRawMany(); // Raw 데이터 가져오기
-
-  //   return result;
-  // }
-
   async tunaTvOnlineList(qr?: QueryRunner) {
-    const result = await this.executeRawQuery();
+    const userTunaTvOnlineRepository = this.getUserTunaTvOnlineRepository(qr);
+    const result = await userTunaTvOnlineRepository
+      .createQueryBuilder('a') // `user_tuna_tv_online` 테이블의 별칭을 `a`로 설정
+      .select('a.tuna_tv_id', 'tunaTvId') // 첫 번째 테이블에서 tuna_tv_id 선택
+      .addSelect('b.ingame_kind', 'ingameKind') // 두 번째 테이블에서 ingame_kind 선택
+      .addSelect('b.select_1', 'select1')
+      .addSelect('b.select_2', 'select2')
+      .addSelect('b.select_3', 'select3')
+      .addSelect('b.score', 'score') // 추가 선택
+      .innerJoin(
+        'user_tuna_tv', // 조인 대상 테이블
+        'b', // 조인 대상 테이블의 별칭
+        'a.tuna_tv_id = b.id', // 조인 조건
+      )
+      .getRawMany(); // Raw 데이터 가져오기
 
     return result;
   }
+
+  // async tunaTvOnlineList(qr?: QueryRunner) {
+  //   const result = await this.executeRawQuery();
+
+  //   return result;
+  // }
 
   async executeRawQuery() {
     const query = `
