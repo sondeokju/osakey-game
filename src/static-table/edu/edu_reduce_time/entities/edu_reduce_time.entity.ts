a3215c312502fcd45749d3b4a62d6653268ebe19
sondeokju <thsejrwn78@naver.com>
2024-11-26 11:07:11 +0900

    log

--- src/static-table/edu/edu_reduce_time/entities/edu_reduce_time.entity.ts
+++ src/static-table/edu/edu_reduce_time/entities/edu_reduce_time.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 @Entity()
 export class EduReduceTime {
   @PrimaryColumn()
-  edu_list_id: number;
+  id: number;
 
   @Column({
     default: '',
