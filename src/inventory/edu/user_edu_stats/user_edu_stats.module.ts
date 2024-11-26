43b9e90c93c3c2ae074624706c4170eb8933ce6a
sondeokju <thsejrwn78@naver.com>
2024-11-26 14:18:09 +0900

    log

--- src/inventory/edu/user_edu_stats/user_edu_stats.module.ts
+++ src/inventory/edu/user_edu_stats/user_edu_stats.module.ts
@@ -5,9 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEduStats } from './entities/user_edu_stats.entity';
 import { EduListService } from 'src/static-table/edu/edu_list/edu_list.service';
 import { EduCurriculumService } from 'src/static-table/edu/edu_curriculum/edu_curriculum.service';
+import { EduList } from 'src/static-table/edu/edu_list/entities/edu_list.entity';
+import { EduCurriculum } from 'src/static-table/edu/edu_curriculum/entities/edu_curriculum.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEduStats])],
+  imports: [TypeOrmModule.forFeature([UserEduStats, EduList, EduCurriculum])],
   exports: [UserEduStatsService],
   controllers: [UserEduStatsController],
   providers: [UserEduStatsService, EduListService, EduCurriculumService],
