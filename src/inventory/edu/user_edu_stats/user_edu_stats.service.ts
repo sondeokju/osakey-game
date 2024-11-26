0d331c75a864a09fe826b220fedb63f7c75f3b73
sondeokju <thsejrwn78@naver.com>
2024-11-26 19:21:49 +0900

    log

--- src/inventory/edu/user_edu_stats/user_edu_stats.service.ts
+++ src/inventory/edu/user_edu_stats/user_edu_stats.service.ts
@@ -174,6 +174,8 @@ export class UserEduStatsService {
       where: { user_id, edu_list_id },
     });
 
+    console.log(userEduStats);
+
     // 새 교육 과정 생성
     if (!userEduStats) {
       return this.createEduLearn(user_id, eduList, qr);
