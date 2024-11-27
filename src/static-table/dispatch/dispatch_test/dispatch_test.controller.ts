c3372cb6dee884d39396ecd0fc0bc3aa458f50d1
sondeokju <thsejrwn78@naver.com>
2024-11-27 15:58:52 +0900

    log

--- src/static-table/dispatch/dispatch_test/dispatch_test.controller.ts
+++ src/static-table/dispatch/dispatch_test/dispatch_test.controller.ts
@@ -1,34 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { DispatchTestService } from './dispatch_test.service';
-import { CreateDispatchTestDto } from './dto/create-dispatch_test.dto';
-import { UpdateDispatchTestDto } from './dto/update-dispatch_test.dto';
 
 @Controller('dispatch-test')
 export class DispatchTestController {
   constructor(private readonly dispatchTestService: DispatchTestService) {}
-
-  @Post()
-  create(@Body() createDispatchTestDto: CreateDispatchTestDto) {
-    return this.dispatchTestService.create(createDispatchTestDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.dispatchTestService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.dispatchTestService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDispatchTestDto: UpdateDispatchTestDto) {
-    return this.dispatchTestService.update(+id, updateDispatchTestDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.dispatchTestService.remove(+id);
-  }
 }
