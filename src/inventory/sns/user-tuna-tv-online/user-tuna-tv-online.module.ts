e195cea96fbac6323c73f6712efd3b11d016a7a8
sondeokju <thsejrwn78@naver.com>
2024-11-27 17:13:46 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.module.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.module.ts
@@ -7,13 +7,25 @@ import { UserTunaTvService } from '../user_tuna_tv/user_tuna_tv.service';
 import { UserTunaTv } from '../user_tuna_tv/entities/user_tuna_tv.entity';
 import { SnsConfigService } from 'src/static-table/sns/sns_config/sns_config.service';
 import { SnsConfig } from 'src/static-table/sns/sns_config/entities/sns_config.entity';
+import { UserSnsLikesService } from '../user_sns_likes/user_sns_likes.service';
+import { UserSnsLikes } from '../user_sns_likes/entities/user_sns_likes.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserTunaTvOnline, UserTunaTv, SnsConfig]),
+    TypeOrmModule.forFeature([
+      UserTunaTvOnline,
+      UserTunaTv,
+      SnsConfig,
+      UserSnsLikes,
+    ]),
   ],
   exports: [UserTunaTvOnlineService],
   controllers: [UserTunaTvOnlineController],
-  providers: [UserTunaTvOnlineService, UserTunaTvService, SnsConfigService],
+  providers: [
+    UserTunaTvOnlineService,
+    UserTunaTvService,
+    SnsConfigService,
+    UserSnsLikesService,
+  ],
 })
 export class UserTunaTvOnlineModule {}
