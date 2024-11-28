33af96fc566f93698437e2dbb55afc2a574a7e5e
sondeokju <thsejrwn78@naver.com>
2024-11-28 14:22:27 +0900

    log

--- src/inventory/sns/user_sns_level/user_sns_level.service.ts
+++ src/inventory/sns/user_sns_level/user_sns_level.service.ts
@@ -39,9 +39,10 @@ export class UserSnsLevelService {
 
       await userSnsLevelRepository.save({
         user_id,
-        // sns_level: 0,
-        // sns_exp: 0,
-        // reward_yn: 'N',
+        sns_level: 0,
+        sns_exp: 0,
+        sns_reward_id: 0,
+        reward_yn: 'N',
       });
 
       //await userSnsLevelRepository.insert(userSnsLevelInsert);
