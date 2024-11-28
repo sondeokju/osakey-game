7edf7adb71f7c33358cae5b1c2644743138a08bf
sondeokju <thsejrwn78@naver.com>
2024-11-28 10:57:46 +0900

    log

--- src/inventory/sns/user_sns_likes/user_sns_likes.controller.ts
+++ src/inventory/sns/user_sns_likes/user_sns_likes.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, UseInterceptors } from '@nestjs/common';
 import { UserSnsLikesService } from './user_sns_likes.service';
+import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { User } from 'src/users/decorator/user.decorator';
+import { QueryRunner } from 'src/common/decorator/query-runner.decorator';
+import { QueryRunner as QR } from 'typeorm';
+import { Users } from 'src/users/entity/users.entity';
 
-@Controller('sns-likes')
+@Controller('like')
 export class UserSnsLikesController {
   constructor(private readonly userSnsLikesService: UserSnsLikesService) {}
+
+  @Get('check')
+  @UseInterceptors(TransactionInterceptor)
+  async TunaTvList(
+    @User() user: Users,
+    @Body('tuna_tv_id') tuna_tv_id: number,
+    @QueryRunner() qr: QR,
+  ) {
+    const result = await this.userSnsLikesService.isLiked(
+      user.id,
+      tuna_tv_id,
+      qr,
+    );
+
+    return JSON.stringify(result);
+  }
 }
