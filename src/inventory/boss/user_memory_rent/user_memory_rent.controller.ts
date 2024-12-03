52e117c6e392ca2d199b9d4553c27427404e1f60
sondeokju <thsejrwn78@naver.com>
2024-12-03 17:21:55 +0900

    log

--- src/inventory/boss/user_memory_rent/user_memory_rent.controller.ts
+++ src/inventory/boss/user_memory_rent/user_memory_rent.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
 import { UserMemoryRentService } from './user_memory_rent.service';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 
-@Controller('user-memory-rent')
+import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { User } from 'src/users/decorator/user.decorator';
+import { QueryRunner } from 'src/common/decorator/query-runner.decorator';
+import { QueryRunner as QR } from 'typeorm';
+import { Users } from 'src/users/entity/users.entity';
+
+@Controller('memory-rent')
 export class UserMemoryRentController {
   constructor(private readonly userMemoryRentService: UserMemoryRentService) {}
+
+  @Post('add')
+  @UseInterceptors(TransactionInterceptor)
+  async followAdd(
+    @User() user: Users,
+    @Body('rent_memory_user_id') rent_memory_user_id: number,
+    @Body('boss_id') boss_id: number,
+    @QueryRunner() qr: QR,
+  ) {
+    const result = await this.userMemoryRentService.memoryRent(
+      user.id,
+      rent_memory_user_id,
+      boss_id,
+      qr,
+    );
+
+    return JSON.stringify(result);
+  }
 }
