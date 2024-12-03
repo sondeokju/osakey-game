2e9f1323fcbf90a8107e785369e569a4b33d1444
sondeokju <thsejrwn78@naver.com>
2024-12-03 18:39:11 +0900

    log

--- src/inventory/boss/user_memory_rent/user_memory_rent.module.ts
+++ src/inventory/boss/user_memory_rent/user_memory_rent.module.ts
@@ -5,11 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserMemoryRent } from './entities/user_memory_rent.entity';
 import { UserMemory } from '../user_memory/entities/user_memory.entity';
 import { UserMemoryService } from '../user_memory/user_memory.service';
+import { UserSnsFollowService } from 'src/inventory/sns/user_sns_follow/user_sns_follow.service';
+import { UserSnsFollow } from 'src/inventory/sns/user_sns_follow/entities/user_sns_follow.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserMemoryRent, UserMemory])],
+  imports: [
+    TypeOrmModule.forFeature([UserMemoryRent, UserMemory, UserSnsFollow]),
+  ],
   exports: [UserMemoryRentService],
   controllers: [UserMemoryRentController],
-  providers: [UserMemoryRentService, UserMemoryService],
+  providers: [UserMemoryRentService, UserMemoryService, UserSnsFollowService],
 })
 export class UserMemoryRentModule {}
