359fef3bc3130671386983ad1ebe85faf20baa74
sondeokju <thsejrwn78@naver.com>
2024-12-03 16:02:32 +0900

    log

--- src/inventory/boss/user_memory/user_memory.controller.ts
+++ src/inventory/boss/user_memory/user_memory.controller.ts
@@ -1,7 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
 import { UserMemoryService } from './user_memory.service';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 
-@Controller('user-memory')
+import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { User } from 'src/users/decorator/user.decorator';
+import { QueryRunner } from 'src/common/decorator/query-runner.decorator';
+import { QueryRunner as QR } from 'typeorm';
+import { Users } from 'src/users/entity/users.entity';
+
+@Controller('memory')
 export class UserMemoryController {
   constructor(private readonly userMemoryService: UserMemoryService) {}
+
+  @Post('add')
+  @UseInterceptors(TransactionInterceptor)
+  async followAdd(
+    @User() user: Users,
+    @Body('boss_id') boss_id: number,
+    @QueryRunner() qr: QR,
+  ) {
+    const result = await this.userMemoryService.memoryAdd(user.id, boss_id, qr);
+
+    return JSON.stringify(result);
+  }
 }
