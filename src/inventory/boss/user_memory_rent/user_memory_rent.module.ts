0c3612cbc8e1426d289c7ff3ae575201eeeb7dbb
sondeokju <thsejrwn78@naver.com>
2024-12-03 18:39:53 +0900

    log

--- src/inventory/boss/user_memory_rent/user_memory_rent.module.ts
+++ src/inventory/boss/user_memory_rent/user_memory_rent.module.ts
@@ -7,13 +7,25 @@ import { UserMemory } from '../user_memory/entities/user_memory.entity';
 import { UserMemoryService } from '../user_memory/user_memory.service';
 import { UserSnsFollowService } from 'src/inventory/sns/user_sns_follow/user_sns_follow.service';
 import { UserSnsFollow } from 'src/inventory/sns/user_sns_follow/entities/user_sns_follow.entity';
+import { SnsConfigService } from 'src/static-table/sns/sns_config/sns_config.service';
+import { SnsConfig } from 'src/static-table/sns/sns_config/entities/sns_config.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserMemoryRent, UserMemory, UserSnsFollow]),
+    TypeOrmModule.forFeature([
+      UserMemoryRent,
+      UserMemory,
+      UserSnsFollow,
+      SnsConfig,
+    ]),
   ],
   exports: [UserMemoryRentService],
   controllers: [UserMemoryRentController],
-  providers: [UserMemoryRentService, UserMemoryService, UserSnsFollowService],
+  providers: [
+    UserMemoryRentService,
+    UserMemoryService,
+    UserSnsFollowService,
+    SnsConfigService,
+  ],
 })
 export class UserMemoryRentModule {}
