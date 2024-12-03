599871364d6cb63bbf650f469d9022ae050ab1e7
sondeokju <thsejrwn78@naver.com>
2024-12-03 15:52:04 +0900

    log

--- src/inventory/boss/user_memory/user_memory.module.ts
+++ src/inventory/boss/user_memory/user_memory.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserMemory } from './entities/user_memory.entity';
 import { UserSnsFollow } from 'src/inventory/sns/user_sns_follow/entities/user_sns_follow.entity';
 import { UserSnsFollowService } from 'src/inventory/sns/user_sns_follow/user_sns_follow.service';
+import { SnsConfigService } from 'src/static-table/sns/sns_config/sns_config.service';
+import { SnsConfig } from 'src/static-table/sns/sns_config/entities/sns_config.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserMemory, UserSnsFollow])],
+  imports: [TypeOrmModule.forFeature([UserMemory, UserSnsFollow, SnsConfig])],
   exports: [UserMemoryService],
   controllers: [UserMemoryController],
-  providers: [UserMemoryService, UserSnsFollowService],
+  providers: [UserMemoryService, UserSnsFollowService, SnsConfigService],
 })
 export class UserMemoryModule {}
