2fa91ccb6dc923e600505674c29f22cbb9134878
sondeokju <thsejrwn78@naver.com>
2024-12-03 16:45:11 +0900

    log

--- src/inventory/boss/user_memory_rent/entities/user_memory_rent.entity.ts
+++ src/inventory/boss/user_memory_rent/entities/user_memory_rent.entity.ts
@@ -1,1 +1,27 @@
-export class UserMemoryRent {}
+import { BaseModel } from 'src/common/entity/base.entity';
+import { Column, Entity, Index } from 'typeorm';
+
+@Entity()
+//@Index('user_memory_user_id_memory', ['user_id', 'boss_id'], { unique: true })
+export class UserMemoryRent extends BaseModel {
+  @Column({
+    default: 0,
+  })
+  @Index({ unique: true })
+  user_id: number;
+
+  @Column({
+    default: '0',
+  })
+  rent_boss_1: number;
+
+  @Column({
+    default: '0',
+  })
+  rent_boss_2: number;
+
+  @Column({
+    default: '0',
+  })
+  rent_boss_3: number;
+}
