dbe1b5b85ca8d13478c595ff930c5149a1e99f01
sondeokju <thsejrwn78@naver.com>
2024-12-04 13:44:47 +0900

    log

--- src/inventory/boss/user_memory_rent/user_memory_rent.service.ts
+++ src/inventory/boss/user_memory_rent/user_memory_rent.service.ts
@@ -84,7 +84,7 @@ export class UserMemoryRentService {
 
   async getUserMemoryRent(user_id: number, qr?: QueryRunner) {
     const userMemoryRentRepository = this.getUserMemoryRentRepository(qr);
-    const userMemoryRent = await userMemoryRentRepository.find({
+    const userMemoryRent = await userMemoryRentRepository.findOne({
       where: {
         user_id,
       },
