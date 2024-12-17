ecc5adc7a8625dd64199ad7ea584c8f652027507
sondeokju <thsejrwn78@naver.com>
2024-12-17 17:03:44 +0900

    log

--- src/inventory/equipment/user_equip_slot/user_equip_slot.module.ts
+++ src/inventory/equipment/user_equip_slot/user_equip_slot.module.ts
@@ -3,11 +3,13 @@ import { UserEquipSlotService } from './user_equip_slot.service';
 import { UserEquipSlotController } from './user_equip_slot.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEquipSlot } from './entities/user_equip_slot.entity';
+import { Equip } from 'src/static-table/equipment/equip/entities/equip.entity';
+import { EquipService } from 'src/static-table/equipment/equip/equip.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEquipSlot])],
+  imports: [TypeOrmModule.forFeature([UserEquipSlot, Equip])],
   exports: [UserEquipSlotService],
   controllers: [UserEquipSlotController],
-  providers: [UserEquipSlotService],
+  providers: [UserEquipSlotService, EquipService],
 })
 export class UserEquipSlotModule {}
