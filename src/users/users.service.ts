05202edf7ee8d347bec79c4ac9dfbd60c1f7e664
sondeokju <thsejrwn78@naver.com>
2025-01-16 14:43:46 +0900

    log

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,3 +1,4 @@
+import { Length } from 'class-validator';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from './entity/users.entity';
