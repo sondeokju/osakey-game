24505a5f173c9c487325555b909e33ec18694854
sondeokju <thsejrwn78@naver.com>
2025-01-17 19:00:44 +0900

    log

--- src/static-table/secame/secame_mail/entities/secame_mail.entity.ts
+++ src/static-table/secame/secame_mail/entities/secame_mail.entity.ts
@@ -1,1 +1,22 @@
-export class SecameMail {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class SecameMail {
+  @PrimaryColumn()
+  secame_mail_id: number;
+
+  @Column({
+    default: 0,
+  })
+  npc_id: number;
+
+  @Column({
+    default: 0,
+  })
+  npc_friendship_point: number;
+
+  @Column({
+    default: 0,
+  })
+  total_exp: number;
+}
