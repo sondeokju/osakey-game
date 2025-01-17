16325de915db3a89a9aa191346d2fd72a3739feb
sondeokju <thsejrwn78@naver.com>
2025-01-17 18:03:16 +0900

    log

--- src/static-table/battlepass/pass_edu/entities/pass_edu.entity.ts
+++ src/static-table/battlepass/pass_edu/entities/pass_edu.entity.ts
@@ -1,1 +1,34 @@
-export class PassEdu {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PassEdu {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    default: 0,
+  })
+  pass_id: number;
+
+  @Column({
+    default: 0,
+  })
+  edu_learn_qty: number;
+
+  @Column({
+    default: 0,
+  })
+  free_reward_id: number;
+
+  @Column({
+    default: 0,
+  })
+  cash_reward_id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    default: '',
+  })
+  is_end: string;
+}
--- src/static-table/battlepass/pass_edu/pass_edu.module.ts
+++ src/static-table/battlepass/pass_edu/pass_edu.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassEduService } from './pass_edu.service';
 import { PassEduController } from './pass_edu.controller';
+import { PassEdu } from './entities/pass_edu.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PassEdu])],
+  exports: [PassEduService],
   controllers: [PassEduController],
   providers: [PassEduService],
 })
