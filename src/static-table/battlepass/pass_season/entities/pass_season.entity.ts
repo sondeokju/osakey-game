68505de248e3260bd0c91368157e6f2331b26b4a
sondeokju <thsejrwn78@naver.com>
2025-01-17 18:12:22 +0900

    log

--- src/static-table/battlepass/pass_season/entities/pass_season.entity.ts
+++ src/static-table/battlepass/pass_season/entities/pass_season.entity.ts
@@ -1,1 +1,34 @@
-export class PassSeason {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PassSeason {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    default: 0,
+  })
+  pass_id: number;
+
+  @Column({
+    default: 0,
+  })
+  unlock_point_qty: number;
+
+  @Column({
+    default: 0,
+  })
+  free_reward_id: number;
+
+  @Column({
+    default: 0,
+  })
+  cash_reward_id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    default: '',
+  })
+  is_end: string;
+}
--- src/static-table/battlepass/pass_season/pass_season.module.ts
+++ src/static-table/battlepass/pass_season/pass_season.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassSeasonService } from './pass_season.service';
 import { PassSeasonController } from './pass_season.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassSeason } from './entities/pass_season.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PassSeason])],
+  exports: [PassSeasonService],
   controllers: [PassSeasonController],
   providers: [PassSeasonService],
 })
