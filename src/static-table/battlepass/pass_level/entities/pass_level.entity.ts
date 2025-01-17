86cf26bf907943a57bd124591ecdb47c86add028
sondeokju <thsejrwn78@naver.com>
2025-01-17 18:06:50 +0900

    log

--- src/static-table/battlepass/pass_level/entities/pass_level.entity.ts
+++ src/static-table/battlepass/pass_level/entities/pass_level.entity.ts
@@ -1,1 +1,34 @@
-export class PassLevel {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PassLevel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    default: 0,
+  })
+  pass_id: number;
+
+  @Column({
+    default: 0,
+  })
+  user_level: number;
+
+  @Column({
+    default: 0,
+  })
+  free_reward_id: number;
+
+  @Column({
+    default: 0,
+  })
+  cash_reward_id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    default: '',
+  })
+  is_end: string;
+}
--- src/static-table/battlepass/pass_level/pass_level.module.ts
+++ src/static-table/battlepass/pass_level/pass_level.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassLevelService } from './pass_level.service';
 import { PassLevelController } from './pass_level.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassLevel } from './entities/pass_level.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PassLevel])],
+  exports: [PassLevelService],
   controllers: [PassLevelController],
   providers: [PassLevelService],
 })
