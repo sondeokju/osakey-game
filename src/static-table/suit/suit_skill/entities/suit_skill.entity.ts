2ff6b681fd0456a006c25ee167854908f70d7302
sondeokju <thsejrwn78@naver.com>
2025-01-17 20:39:40 +0900

    log

--- src/static-table/suit/suit_skill/entities/suit_skill.entity.ts
+++ src/static-table/suit/suit_skill/entities/suit_skill.entity.ts
@@ -1,1 +1,27 @@
-export class SuitSkill {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class SuitSkill {
+  @PrimaryColumn()
+  id: number;
+
+  @Column({
+    default: 0,
+  })
+  suit_info_id: number;
+
+  @Column({
+    default: 0,
+  })
+  unlock_level: number;
+
+  @Column({
+    default: 0,
+  })
+  is_passive: number;
+
+  @Column({
+    default: 0,
+  })
+  skill_id: number;
+}
