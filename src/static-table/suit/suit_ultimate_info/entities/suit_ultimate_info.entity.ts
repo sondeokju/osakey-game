54d7800cd3d561440619bc063e446b20e43b76dd
sondeokju <thsejrwn78@naver.com>
2025-01-17 20:43:30 +0900

    log

--- src/static-table/suit/suit_ultimate_info/entities/suit_ultimate_info.entity.ts
+++ src/static-table/suit/suit_ultimate_info/entities/suit_ultimate_info.entity.ts
@@ -1,1 +1,34 @@
-export class SuitUltimateInfo {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class SuitUltimateInfo {
+  @PrimaryColumn()
+  suit_ultimate_info_id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    default: '',
+  })
+  ultimate_type: string;
+
+  @Column({
+    default: 0,
+  })
+  duration: number;
+
+  @Column({
+    default: 0,
+  })
+  max_gauge_value: number;
+
+  @Column({
+    default: 0,
+  })
+  gauge_gauge_value: number;
+
+  @Column({
+    default: 0,
+  })
+  max_stack_count: number;
+}
