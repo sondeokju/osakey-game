1bd029c5e92772b035aba82f2257d60a467f078d
sondeokju <thsejrwn78@naver.com>
2025-01-17 20:30:31 +0900

    log

--- src/static-table/suit/suit_level_info/entities/suit_level_info.entity.ts
+++ src/static-table/suit/suit_level_info/entities/suit_level_info.entity.ts
@@ -1,32 +1,4 @@
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
-export class SuitLeveInfo {
-  @PrimaryColumn()
-  suit_info_id: number;
-
-  @Column({
-    default: 0,
-  })
-  grade: number;
-
-  @Column({
-    default: 0,
-  })
-  ultimate_index: number;
-
-  @Column({
-    default: 0,
-  })
-  suit_piece_id: number;
-
-  @Column({
-    default: 0,
-  })
-  unlock_piece_count: number;
-
-  @Column({
-    default: 0,
-  })
-  unlock_main_mission: number;
-}
+export class SuitLevelInfo {}
