caab1fc60d2f71a36df4d3a798678579827c321c
sondeokju <thsejrwn78@naver.com>
2025-01-17 17:59:46 +0900

    log

--- src/static-table/battlepass/pass_bounty/pass_bounty.module.ts
+++ src/static-table/battlepass/pass_bounty/pass_bounty.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassBountyService } from './pass_bounty.service';
 import { PassBountyController } from './pass_bounty.controller';
+import { PassBounty } from './entities/pass_bounty.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PassBounty])],
+  exports: [PassBountyService],
   controllers: [PassBountyController],
   providers: [PassBountyService],
 })
