a6e46c740fedea865602564b0c087371966a2e9c
sondeokju <thsejrwn78@naver.com>
2025-01-17 18:09:46 +0900

    log

--- src/static-table/battlepass/pass_mission/entities/pass_mission.entity.ts
+++ src/static-table/battlepass/pass_mission/entities/pass_mission.entity.ts
@@ -1,1 +1,34 @@
-export class PassMission {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PassMission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    default: 0,
+  })
+  pass_id: number;
+
+  @Column({
+    default: 0,
+  })
+  mission_clear_qty: number;
+
+  @Column({
+    default: 0,
+  })
+  free_reward_id: number;
+
+  @Column({
+    default: 0,
+  })
+  cash_reward_id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    default: '',
+  })
+  is_end: string;
+}
--- src/static-table/battlepass/pass_mission/pass_mission.module.ts
+++ src/static-table/battlepass/pass_mission/pass_mission.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassMissionService } from './pass_mission.service';
 import { PassMissionController } from './pass_mission.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassMission } from './entities/pass_mission.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PassMission])],
+  exports: [PassMissionService],
   controllers: [PassMissionController],
   providers: [PassMissionService],
 })
