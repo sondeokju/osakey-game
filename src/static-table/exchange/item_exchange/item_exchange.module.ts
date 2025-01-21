a2aff05d43d1134c7479588d268485fe73275a91
sondeokju <thsejrwn78@naver.com>
2025-01-21 14:36:11 +0900

    log

--- src/static-table/exchange/item_exchange/item_exchange.module.ts
+++ src/static-table/exchange/item_exchange/item_exchange.module.ts
@@ -3,9 +3,10 @@ import { ItemExchangeService } from './item_exchange.service';
 import { ItemExchangeController } from './item_exchange.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemExchange } from './entities/item_exchange.entity';
+import { RewardOfferModule } from 'src/supervisor/reward_offer/reward_offer.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ItemExchange])],
+  imports: [TypeOrmModule.forFeature([ItemExchange]), RewardOfferModule],
   exports: [ItemExchangeService],
   controllers: [ItemExchangeController],
   providers: [ItemExchangeService],
