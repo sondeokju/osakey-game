4ceef534f0321d577e82921dc3b31ce392e10db0
sondeokju <thsejrwn78@naver.com>
2025-01-21 11:07:15 +0900

    log

--- src/inventory/exchange/user_item_exchange/entities/user_item_exchange.entity.ts
+++ src/inventory/exchange/user_item_exchange/entities/user_item_exchange.entity.ts
@@ -1,1 +1,44 @@
-export class UserItemExchange {}
+import { BaseModel } from 'src/common/entity/base.entity';
+import { Column, Entity, Index } from 'typeorm';
+
+@Entity()
+//@Index('IDX_NAME', ['columnA', 'columnB'], { unique: true }) // 복합 인덱스 설정
+export class UserItemExchange extends BaseModel {
+  @Column({
+    nullable: false,
+    type: 'char',
+    length: 10,
+  })
+  @Index({ unique: false })
+  user_id: string;
+
+  @Column({
+    default: '0',
+  })
+  exchange_id: number;
+
+  @Column({
+    default: '0',
+  })
+  exchange_count: number;
+
+  @Column({
+    default: '0',
+  })
+  result_exchange_id: number;
+
+  @Column({
+    default: '0',
+  })
+  result_exchange_count: number;
+
+  @Column({
+    type: 'datetime',
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: string) => new Date(value),
+    },
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  exchange_date: Date;
+}
--- src/inventory/exchange/user_item_exchange/user_item_exchange.module.ts
+++ src/inventory/exchange/user_item_exchange/user_item_exchange.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserItemExchangeService } from './user_item_exchange.service';
 import { UserItemExchangeController } from './user_item_exchange.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserItemexchange } from './entities/user_item_exchange.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserItemexchange])],
+  exports: [UserItemExchangeService],
   controllers: [UserItemExchangeController],
   providers: [UserItemExchangeService],
 })
