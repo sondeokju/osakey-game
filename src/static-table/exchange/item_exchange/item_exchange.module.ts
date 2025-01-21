5409f0308890a9dcf81a6c07d3e96a31cc2b3aa3
sondeokju <thsejrwn78@naver.com>
2025-01-21 14:40:23 +0900

    log

--- src/static-table/exchange/item_exchange/item_exchange.module.ts
+++ src/static-table/exchange/item_exchange/item_exchange.module.ts
@@ -6,15 +6,37 @@ import { ItemExchange } from './entities/item_exchange.entity';
 import { RewardOfferModule } from 'src/supervisor/reward_offer/reward_offer.module';
 import { RewardOfferService } from 'src/supervisor/reward_offer/reward_offer.service';
 import { RewardModule } from 'src/static-table/reward/reward.module';
+import { Hero } from 'src/static-table/hero/entities/hero.entity';
+import { HeroService } from 'src/static-table/hero/hero.service';
+import { Item } from 'src/static-table/item/entities/item.entity';
+import { ItemService } from 'src/static-table/item/item.service';
+import { Reward } from 'src/static-table/reward/entities/reward.entity';
+import { RewardService } from 'src/static-table/reward/reward.service';
+import { UserItem } from 'src/user_item/entities/user_item.entity';
+import { UserItemService } from 'src/user_item/user_item.service';
+import { Users } from 'src/users/entity/users.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ItemExchange]),
-    RewardOfferModule,
-    RewardModule,
+    TypeOrmModule.forFeature([
+      ItemExchange,
+      Reward,
+      Users,
+      Item,
+      UserItem,
+      Hero,
+    ]),
   ],
   exports: [ItemExchangeService],
   controllers: [ItemExchangeController],
-  providers: [ItemExchangeService, RewardOfferService],
+  providers: [
+    ItemExchangeService,
+    RewardService,
+    UsersService,
+    ItemService,
+    UserItemService,
+    HeroService,
+  ],
 })
 export class ItemExchangeModule {}
