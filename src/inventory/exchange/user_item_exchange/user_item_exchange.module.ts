647b1d920fc38016bae83d18e0d132034c2b4c82
sondeokju <thsejrwn78@naver.com>
2025-01-21 14:47:17 +0900

    log

--- src/inventory/exchange/user_item_exchange/user_item_exchange.module.ts
+++ src/inventory/exchange/user_item_exchange/user_item_exchange.module.ts
@@ -4,9 +4,14 @@ import { UserItemExchangeController } from './user_item_exchange.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserItemExchange } from './entities/user_item_exchange.entity';
 import { RewardOfferModule } from 'src/supervisor/reward_offer/reward_offer.module';
+import { ItemExchangeModule } from 'src/static-table/exchange/item_exchange/item_exchange.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserItemExchange]), RewardOfferModule],
+  imports: [
+    TypeOrmModule.forFeature([UserItemExchange]),
+    RewardOfferModule,
+    ItemExchangeModule,
+  ],
   exports: [UserItemExchangeService],
   controllers: [UserItemExchangeController],
   providers: [UserItemExchangeService],
