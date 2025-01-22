91711e4f6406f156f528b5251a5ac61ad9368da3
sondeokju <thsejrwn78@naver.com>
2025-01-22 10:52:55 +0900

    log

--- src/supervisor/resource_manager/resource_manager.module.ts
+++ src/supervisor/resource_manager/resource_manager.module.ts
@@ -5,9 +5,10 @@ import { UserItemService } from 'src/user_item/user_item.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/users/entity/users.entity';
 import { HeroModule } from 'src/static-table/hero/hero.module';
+import { UserItemModule } from 'src/user_item/user_item.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users]), HeroModule],
+  imports: [TypeOrmModule.forFeature([Users]), HeroModule, UserItemModule],
   providers: [ResourceManagerService, UsersService, UserItemService],
   exports: [ResourceManagerService],
 })
