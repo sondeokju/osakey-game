dc8510e19b28102aaa919316d5aac7ab9c9bbd01
sondeokju <thsejrwn78@naver.com>
2025-01-23 14:29:00 +0900

    log

--- src/inventory/mail/user_mail/user_mail.service.ts
+++ src/inventory/mail/user_mail/user_mail.service.ts
@@ -27,6 +27,53 @@ export class UserMailService {
       : this.userMailRepository;
   }
 
+  async rewardAll_YN(user_id: string, user_mail_id: number, qr?: QueryRunner) {
+    if (!user_id || typeof user_id !== 'string') {
+      throw new BadRequestException('Invalid user_id provided.');
+    }
+
+    const queryRunner = qr || this.dataSource.createQueryRunner();
+
+    let isTransactionOwner = false;
+    if (!qr) {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+      isTransactionOwner = true;
+    }
+
+    try {
+      const userMailRepository = queryRunner.manager.getRepository(UserMail);
+
+      if (isTransactionOwner) {
+        await queryRunner.commitTransaction();
+      }
+
+      const userMailData = await userMailRepository.find({
+        where: { user_id },
+      });
+
+      const updatedUserMailData = userMailData.map((mail) => ({
+        ...mail,
+        reward_yn: 'Y',
+      }));
+
+      const updatedUserMail =
+        await userMailRepository.save(updatedUserMailData);
+
+      return updatedUserMail;
+    } catch (error) {
+      if (isTransactionOwner) {
+        await queryRunner.rollbackTransaction();
+      }
+      console.error('Transaction failed:', error);
+      throw new Error(`Transaction failed: ${error.message}`);
+    } finally {
+      if (isTransactionOwner) {
+        await queryRunner.release();
+      }
+    }
+  }
+
   async removeYN(user_id: string, user_mail_id: number, qr?: QueryRunner) {
     if (!user_id || typeof user_id !== 'string') {
       throw new BadRequestException('Invalid user_id provided.');
