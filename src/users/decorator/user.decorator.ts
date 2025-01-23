4051912292a2e4fc480e52e7200d7e6464bf6585
sondeokju <thsejrwn78@naver.com>
2025-01-23 10:46:11 +0900

    log

--- src/users/decorator/user.decorator.ts
+++ src/users/decorator/user.decorator.ts
@@ -10,6 +10,7 @@ export const User = createParamDecorator(
     const req = context.switchToHttp().getRequest();
 
     const user = req.user as Users;
+    console.log('createParamDecorator', req.user);
 
     if (!user) {
       throw new InternalServerErrorException(
