fa05e7469e38f3ef5edf0d3e2bc29ecac7a238b0
sondeokju <thsejrwn78@naver.com>
2025-01-24 11:51:22 +0900

    log

--- src/inventory/equipment/user_equip/user_equip.service.ts
+++ src/inventory/equipment/user_equip/user_equip.service.ts
@@ -585,7 +585,13 @@ export class UserEquipService {
         ) // 순서를 명시적으로 지정
         .getRawMany();
 
-      console.log('equipIdList', equipIdList);
+      const userEquipData = await userEquipRepository.findOne({
+        where: {
+          id: user_equip_id_01,
+        },
+      });
+
+      console.log('userEquipData', userEquipData);
       const equipIds = equipIdList.map((item) => item.equip_id);
       const deleteEquipIds = equipIdList.map((item) => item.id);
 
