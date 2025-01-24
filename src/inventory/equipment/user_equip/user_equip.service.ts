8b34a578e15459d3e987306d224bcb85edbc12de
sondeokju <thsejrwn78@naver.com>
2025-01-24 10:54:20 +0900

    log

--- src/inventory/equipment/user_equip/user_equip.service.ts
+++ src/inventory/equipment/user_equip/user_equip.service.ts
@@ -585,7 +585,7 @@ export class UserEquipService {
         ) // 순서를 명시적으로 지정
         .getRawMany();
 
-      const equipIds = equipIdList.map((item) => item.equip_id);
+      const equipIds = equipIdList.map((item) => item.id);
 
       // 배열 길이 검증
       if (equipIds.length < 3) {
@@ -615,7 +615,7 @@ export class UserEquipService {
       // 기존 장비 삭제
       await userEquipRepository.delete({
         user_id,
-        equip_id: In(equipIds),
+        id: In(equipIds),
       });
 
       console.log('equipIds', equipIds);
