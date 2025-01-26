848b1838292c5bf922992e0f46c3522d2f66e806
sondeokju <thsejrwn78@naver.com>
2025-01-26 20:35:16 +0900

    log

--- src/inventory/dispatch/user_dispatch/user_dispatch.module.ts
+++ src/inventory/dispatch/user_dispatch/user_dispatch.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserDispatchService } from './user_dispatch.service';
 import { UserDispatchController } from './user_dispatch.controller';
+import { UserDispatch } from './entities/user_dispatch.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserDispatch])],
+  exports: [UserDispatchService],
   controllers: [UserDispatchController],
   providers: [UserDispatchService],
 })
