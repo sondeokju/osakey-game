da0039fb196b1893b7ee791ddcf5630921ba1ef6
sondeokju <thsejrwn78@naver.com>
2025-01-30 13:28:38 +0900

    log

--- src/supervisor/user_setting/user_setting.controller.ts
+++ src/supervisor/user_setting/user_setting.controller.ts
@@ -1,34 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UserSettingService } from './user_setting.service';
-import { CreateUserSettingDto } from './dto/create-user_setting.dto';
-import { UpdateUserSettingDto } from './dto/update-user_setting.dto';
 
 @Controller('user-setting')
 export class UserSettingController {
   constructor(private readonly userSettingService: UserSettingService) {}
-
-  @Post()
-  create(@Body() createUserSettingDto: CreateUserSettingDto) {
-    return this.userSettingService.create(createUserSettingDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.userSettingService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userSettingService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserSettingDto: UpdateUserSettingDto) {
-    return this.userSettingService.update(+id, updateUserSettingDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userSettingService.remove(+id);
-  }
 }
