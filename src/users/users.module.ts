22e623d68af5b10919f4118f177081f596e993b0
sondeokju <thsejrwn78@naver.com>
2025-01-30 13:17:44 +0900

    log

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,9 +7,10 @@ import { Hero } from 'src/static-table/hero/entities/hero.entity';
 import { HeroService } from 'src/static-table/hero/hero.service';
 import { UserMissionModule } from 'src/inventory/mission/user_mission/user_mission.module';
 import { UserMissionService } from 'src/inventory/mission/user_mission/user_mission.service';
+import { UserMission } from 'src/inventory/mission/user_mission/entities/user_mission.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users, Hero]), UserMissionModule],
+  imports: [TypeOrmModule.forFeature([Users, Hero, UserMission])],
   exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService, HeroService, UserMissionService],
