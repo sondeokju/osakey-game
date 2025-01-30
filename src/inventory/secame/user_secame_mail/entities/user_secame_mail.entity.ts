c786b78faf8dcb8ee9a694e748fa3bf6434d39de
sondeokju <thsejrwn78@naver.com>
2025-01-30 19:20:06 +0900

    log

--- src/inventory/secame/user_secame_mail/entities/user_secame_mail.entity.ts
+++ src/inventory/secame/user_secame_mail/entities/user_secame_mail.entity.ts
@@ -4,13 +4,13 @@ import { BaseModel } from 'src/common/entity/base.entity';
 @Entity()
 //@Index('IDX_user_mail_id_user_id', ['id', 'user_id'], { unique: true }) // 복합 인덱스 설정
 export class UserSecameMail extends BaseModel {
-  //   @Column({
-  //     nullable: false,
-  //     type: 'char',
-  //     length: 10,
-  //   })
-  //   @Index({ unique: false })
-  //   user_id: string;
+  @Column({
+    nullable: false,
+    type: 'char',
+    length: 10,
+  })
+  @Index({ unique: false })
+  user_id: string;
 
   @Column({
     default: 0,
