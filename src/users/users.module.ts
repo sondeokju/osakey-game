955bebac8f9773d32be0f551cc99097c0adba18d
sondeokju <thsejrwn78@naver.com>
2025-01-30 13:33:37 +0900

    log

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,11 +6,12 @@ import { Users } from './entity/users.entity';
 import { Hero } from 'src/static-table/hero/entities/hero.entity';
 import { HeroService } from 'src/static-table/hero/hero.service';
 import { UserSettingModule } from 'src/supervisor/user_setting/user_setting.module';
+import { UserSettingService } from 'src/supervisor/user_setting/user_setting.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users, Hero]), UserSettingModule],
   exports: [UsersService],
   controllers: [UsersController],
-  providers: [UsersService, HeroService],
+  providers: [UsersService, HeroService, UserSettingService],
 })
 export class UsersModule {}
