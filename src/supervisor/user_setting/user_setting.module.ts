73f8b68103629101b6257ce3c7ff2d6a0e3b6db4
sondeokju <thsejrwn78@naver.com>
2025-01-30 13:41:36 +0900

    log

--- src/supervisor/user_setting/user_setting.module.ts
+++ src/supervisor/user_setting/user_setting.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserSettingService } from './user_setting.service';
 import { UserSettingController } from './user_setting.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserMissionModule } from 'src/inventory/mission/user_mission/user_mission.module';
+import { UserMission } from 'src/inventory/mission/user_mission/entities/user_mission.entity';
+import { UserMissionService } from 'src/inventory/mission/user_mission/user_mission.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([]), UserMissionModule],
+  imports: [TypeOrmModule.forFeature([UserMission])],
   exports: [UserSettingService],
   controllers: [UserSettingController],
-  providers: [UserSettingService],
+  providers: [UserSettingService, UserMissionService],
 })
 export class UserSettingModule {}
