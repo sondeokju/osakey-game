b7682c2d159f3e8d352cc77132c3845900035acc
sondeokju <thsejrwn78@naver.com>
2025-01-31 10:55:11 +0900

    log

--- src/inventory/suit/user_suit/user_suit.module.ts
+++ src/inventory/suit/user_suit/user_suit.module.ts
@@ -1,8 +1,12 @@
+import { UserSuit } from 'src/inventory/suit/user_suit/entities/user_suit.entity';
 import { Module } from '@nestjs/common';
 import { UserSuitService } from './user_suit.service';
 import { UserSuitController } from './user_suit.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserSuit])],
+  exports: [UserSuitService],
   controllers: [UserSuitController],
   providers: [UserSuitService],
 })
