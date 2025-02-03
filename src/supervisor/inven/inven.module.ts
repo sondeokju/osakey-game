e53156e68e8a5a36f3af524df90d3bd9b848f58c
sondeokju <thsejrwn78@naver.com>
2025-02-03 18:01:41 +0900

    log

--- src/supervisor/inven/inven.module.ts
+++ src/supervisor/inven/inven.module.ts
@@ -4,7 +4,7 @@ import { InvenController } from './inven.controller';
 //import { UserSuitModule } from 'src/inventory/suit/user_suit/user_suit.module';
 import { entities_module } from 'src/entity_group/entity_module';
 
-console.log('entities_module:', entities_module);
+console.log('[DEBUG] imported entities_module:', entities_module);
 console.log('Type:', typeof entities_module);
 console.log('Is Array:', Array.isArray(entities_module));
 
