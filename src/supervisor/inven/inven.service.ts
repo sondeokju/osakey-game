51f3e777fd725b68afd2648a011d09ab6a475e9c
sondeokju <thsejrwn78@naver.com>
2025-02-03 17:54:27 +0900

    log

--- src/supervisor/inven/inven.service.ts
+++ src/supervisor/inven/inven.service.ts
@@ -9,15 +9,12 @@ import { QueryRunner, Repository } from 'typeorm';
 
 @Injectable()
 export class InvenService {
-  constructor(private readonly userSuitService: UserSuitService) {}
-
-  async getUserInventoryAll(user_id: string, qr?: QueryRunner) {
-    const suit = await this.userSuitService.getUserSuit(user_id, qr);
-
-    const inven = {
-      suit,
-    };
-
-    return inven;
-  }
+  // constructor(private readonly userSuitService: UserSuitService) {}
+  // async getUserInventoryAll(user_id: string, qr?: QueryRunner) {
+  //   const suit = await this.userSuitService.getUserSuit(user_id, qr);
+  //   const inven = {
+  //     suit,
+  //   };
+  //   return inven;
+  // }
 }
