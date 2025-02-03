538132e12d2f8a020de0f8ad0a46df6dd5cc16e2
sondeokju <thsejrwn78@naver.com>
2025-02-03 17:52:59 +0900

    log

--- src/supervisor/inven/inven.module.ts
+++ src/supervisor/inven/inven.module.ts
@@ -4,8 +4,12 @@ import { InvenController } from './inven.controller';
 //import { UserSuitModule } from 'src/inventory/suit/user_suit/user_suit.module';
 import { entities_module } from 'src/entity_group/entity_module';
 
+console.log('entities_module:', entities_module);
+console.log('Type:', typeof entities_module);
+console.log('Is Array:', Array.isArray(entities_module));
+
 @Module({
-  imports: [...entities_module],
+  imports: [],
   exports: [InvenService],
   controllers: [InvenController],
   providers: [InvenService],
