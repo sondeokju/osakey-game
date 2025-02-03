d24a3550696fcaf668a4d9213ade396adbec660a
sondeokju <thsejrwn78@naver.com>
2025-02-03 19:12:10 +0900

    log

--- src/supervisor/inven/inven.module.ts
+++ src/supervisor/inven/inven.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { InvenService } from './inven.service';
 import { InvenController } from './inven.controller';
 //import { UserSuitModule } from 'src/inventory/suit/user_suit/user_suit.module';
-import { entities_module } from 'src/entity_group/entity_module';
+//import { entities_module } from 'src/entity_group/entity_module';
+import { entities_module } from '../entity_group/entity_module';
 
 console.log('[DEBUG] imported entities_module:', entities_module);
 console.log('Type:', typeof entities_module);
