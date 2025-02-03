26381847c6ab04869751667521bbf82a40734eab
sondeokju <thsejrwn78@naver.com>
2025-02-03 20:47:36 +0900

    log

--- src/inventory/battlepass/user_battle_pass/user_battle_pass.module.ts
+++ src/inventory/battlepass/user_battle_pass/user_battle_pass.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserBattlePassService } from './user_battle_pass.service';
 import { UserBattlePassController } from './user_battle_pass.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserBattlePass } from './entities/user_battle_pass.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserBattlePass])],
+  exports: [UserBattlePassService],
   controllers: [UserBattlePassController],
   providers: [UserBattlePassService],
 })
--- src/inventory/battlepass/user_battle_pass_reward/user_battle_pass_reward.module.ts
+++ src/inventory/battlepass/user_battle_pass_reward/user_battle_pass_reward.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserBattlePassRewardService } from './user_battle_pass_reward.service';
 import { UserBattlePassRewardController } from './user_battle_pass_reward.controller';
+import { UserBattlePassReward } from './entities/user_battle_pass_reward.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserBattlePassReward])],
+  exports: [UserBattlePassRewardService],
   controllers: [UserBattlePassRewardController],
   providers: [UserBattlePassRewardService],
 })
