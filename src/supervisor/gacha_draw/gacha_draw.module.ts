6c3cb18eadb6b22a8425ac037c21829bbda93c61
sondeokju <thsejrwn78@naver.com>
2025-02-04 18:35:52 +0900

    log

--- src/supervisor/gacha_draw/gacha_draw.module.ts
+++ src/supervisor/gacha_draw/gacha_draw.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GachaDrawService } from './gacha_draw.service';
 import { GachaDrawController } from './gacha_draw.controller';
+import { RewardOfferModule } from '../reward_offer/reward_offer.module';
 
 @Module({
-  imports: [],
+  imports: [RewardOfferModule],
   exports: [GachaDrawService],
   controllers: [GachaDrawController],
   providers: [GachaDrawService],
