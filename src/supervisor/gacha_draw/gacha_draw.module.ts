0a137db11915f14856f66a598e8beea3c1cee598
sondeokju <thsejrwn78@naver.com>
2025-02-04 19:12:43 +0900

    log

--- src/supervisor/gacha_draw/gacha_draw.module.ts
+++ src/supervisor/gacha_draw/gacha_draw.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { GachaDrawService } from './gacha_draw.service';
 import { GachaDrawController } from './gacha_draw.controller';
 import { RewardOfferModule } from '../reward_offer/reward_offer.module';
+import { ItemModule } from 'src/static-table/item/item.module';
+import { GachaModule } from 'src/static-table/draw/gacha/gacha.module';
+import { GachaOutputModule } from 'src/static-table/draw/gacha_output/gacha_output.module';
 
 @Module({
-  imports: [RewardOfferModule],
+  imports: [RewardOfferModule, GachaModule, GachaOutputModule],
   exports: [GachaDrawService],
   controllers: [GachaDrawController],
   providers: [GachaDrawService],
--- src/supervisor/gacha_draw/gacha_draw.service.ts
+++ src/supervisor/gacha_draw/gacha_draw.service.ts
@@ -8,11 +8,17 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, QueryRunner, Repository } from 'typeorm';
 import { RewardOfferService } from 'src/supervisor/reward_offer/reward_offer.service';
+//import { ItemService } from 'src/static-table/item/item.service';
+import { GachaService } from 'src/static-table/draw/gacha/gacha.service';
+import { GachaOutputService } from 'src/static-table/draw/gacha_output/gacha_output.service';
 
 @Injectable()
 export class GachaDrawService {
   constructor(
     private readonly rewardOfferService: RewardOfferService,
+    //private readonly itemService: ItemService,
+    private readonly gachaService: GachaService,
+    private readonly gachaOutputService: GachaOutputService,
     private readonly dataSource: DataSource,
   ) {}
 }
