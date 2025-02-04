8b55b5f7d49918b412f938d7736db9c5eff6ac68
sondeokju <thsejrwn78@naver.com>
2025-02-04 19:07:07 +0900

    log

--- src/static-table/item/entities/item.entity.ts
+++ src/static-table/item/entities/item.entity.ts
@@ -5,12 +5,29 @@ export class Item {
   @PrimaryColumn()
   item_id: number;
 
-  @Column({})
+  @Column({
+    type: 'varchar',
+    length: 10,
+    default: '',
+  })
   item_type: string;
 
-  @Column({})
+  @Column({
+    default: 0,
+  })
   item_grade: number;
 
-  @Column({})
+  @Column({
+    type: 'varchar',
+    length: 128,
+    default: '',
+  })
+  item_enum: string;
+
+  @Column({
+    type: 'varchar',
+    length: 128,
+    default: '',
+  })
   item_name: string;
 }
