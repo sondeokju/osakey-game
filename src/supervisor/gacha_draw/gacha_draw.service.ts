305c492857b0a2a3a3335abb5c53c86e753f2be5
sondeokju <thsejrwn78@naver.com>
2025-02-05 17:54:06 +0900

    log

--- src/supervisor/gacha_draw/gacha_draw.service.ts
+++ src/supervisor/gacha_draw/gacha_draw.service.ts
@@ -51,60 +51,168 @@ export class GachaDrawService {
       gacha_id,
       qr,
     );
-
     const totalWeight = gachaList.reduce(
       (sum, gacha) => sum + gacha.item_rate,
       0,
     );
-
-    //const selectedItems: number[] = [];
-
     let random = Math.random() * totalWeight;
+
     for (const gacha of gachaList) {
-      if (random < gacha.item_rate) {
+      random -= gacha.item_rate;
+      if (random <= 0) {
         return Array(gacha.item_qty).fill(gacha.item_id);
       }
-      random -= gacha.item_rate;
     }
-
-    return Array(gachaList[gachaList.length - 1].item_qty).fill(
-      gachaList[gachaList.length - 1].item_id,
-    );
+    return [];
   }
 
+  async itemGradeCheck(items: number[], item_grade: number, qr?: QueryRunner) {
+    const itemDataList = await Promise.all(
+      items.map((item) => this.itemService.getItem(item, qr)),
+    );
+    return itemDataList.some((itemData) => itemData.item_grade === item_grade);
+  }
+
+  async itemGradeRandom(
+    gacha_id: number,
+    item_grade: number,
+    qr?: QueryRunner,
+  ) {
+    const query = `
+        SELECT go.gacha_id, go.item_id, go.item_kind,
+               CASE
+                   WHEN go.item_kind = 'E' THEN e.equip_grade
+                   WHEN go.item_kind = 'M' THEN i.item_grade
+               END AS grade,
+               e.equip_name,
+               i.item_name
+        FROM gacha_output go
+        LEFT JOIN equip e ON go.item_kind = 'E' AND go.item_id = e.equip_id
+        LEFT JOIN item i ON go.item_kind = 'M' AND go.item_id = i.item_id
+        WHERE go.gacha_id = ?
+        AND (
+            (go.item_kind = 'E' AND e.equip_grade = ?)
+            OR (go.item_kind = 'M' AND i.item_grade = ?)
+        )
+        ORDER BY RAND()
+        LIMIT 1;
+    `;
+
+    const params = [gacha_id, item_grade, item_grade];
+
+    let result;
+    if (qr) {
+      result = await qr.query(query, params);
+    } else {
+      result = await this.dataSource.query(query, params);
+    }
+
+    return result.length ? result[0] : null;
+  }
   async equipGachaDrawRandom(
     user_id: string,
     gacha_id: number,
     qr?: QueryRunner,
   ) {
-    let rewardData = {};
     const gachaItem = await this.calculEquipGachaDrawRandom(gacha_id);
+    const gachaCheckData =
+      await this.userGachaCheckService.getGachaDrawItemGrade(user_id, qr);
+    const gachaCostData = await this.gachaService.getGacha(gacha_id, qr);
+
+    const [grade4, grade5] = await Promise.all([
+      this.itemGradeCheck(gachaItem, gachaCostData.fixed_item_grade_1, qr),
+      this.itemGradeCheck(gachaItem, gachaCostData.fixed_item_grade_2, qr),
+    ]);
+
+    for (const item of gachaItem) {
+      const itemData = await this.itemService.getItem(item, qr);
+      if (itemData.item_grade === gachaCostData.fixed_item_grade_1) {
+        await this.userGachaCheckService.gachaDrawItemGradeSave(
+          user_id,
+          gachaCostData.fixed_item_grade_1,
+          1,
+          qr,
+        );
+      } else if (itemData.item_grade === gachaCostData.fixed_item_grade_2) {
+        await this.userGachaCheckService.gachaDrawItemGradeSave(
+          user_id,
+          gachaCostData.fixed_item_grade_2,
+          1,
+          qr,
+        );
+      }
+    }
+
+    if (!grade4) {
+      const gradeRandomData = await this.itemGradeRandom(
+        gacha_id,
+        gachaCostData.fixed_item_grade_1,
+        qr,
+      );
+      if (gradeRandomData) {
+        // 기존 아이템 중 무작위로 하나 선택하여 교체
+        const replaceIndex = Math.floor(Math.random() * gachaItem.length);
+        gachaItem[replaceIndex] = gradeRandomData.item_id;
+      }
+    }
+
+    if (!grade5) {
+      const gradeRandomData = await this.itemGradeRandom(
+        gacha_id,
+        gachaCostData.fixed_item_grade_2,
+        qr,
+      );
+      if (gradeRandomData) {
+        // 기존 아이템 중 무작위로 하나 선택하여 교체
+        const replaceIndex = Math.floor(Math.random() * gachaItem.length);
+        gachaItem[replaceIndex] = gradeRandomData.item_id;
+      }
+    }
+
+    if (
+      gachaCheckData.fixed_1_draw_count >=
+      gachaCostData.fixed_item_grade_1_count
+    ) {
+      await this.userGachaCheckService.gachaDrawItemGradeReset(
+        user_id,
+        gachaCostData.fixed_item_grade_1,
+      );
+    }
+
+    if (
+      gachaCheckData.fixed_2_draw_count >=
+      gachaCostData.fixed_item_grade_2_count
+    ) {
+      await this.userGachaCheckService.gachaDrawItemGradeReset(
+        user_id,
+        gachaCostData.fixed_item_grade_2,
+      );
+    }
 
     const gachaList = await this.gachaOutputService.getGachaOutputList(
       gacha_id,
       qr,
     );
 
-    console.log('gachaItem', gachaItem);
-
+    let rewardData;
     if (['E'].includes(gachaList[0].item_kind)) {
-      console.log('item_kind', gachaList[0].item_kind);
       rewardData = await this.rewardOfferService.rewardEquipArray(
         user_id,
         gachaItem,
         qr,
       );
     } else if (['M', 'S'].includes(gachaList[0].item_kind)) {
-      await this.rewardOfferService.rewardSameItemArray(user_id, gachaItem);
+      rewardData = await this.rewardOfferService.rewardSameItemArray(
+        user_id,
+        gachaItem,
+        qr,
+      );
     }
 
     if (!rewardData) {
       throw new BadRequestException('Failed to process reward.');
     }
 
-    return {
-      gachaItem,
-      rewardData,
-    };
+    return { gachaItem, rewardData };
   }
 }
