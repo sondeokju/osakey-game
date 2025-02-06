abf757003ecf22fba0fccc258b928cda9dd8e5dd
sondeokju <thsejrwn78@naver.com>
2025-02-06 15:55:44 +0900

    log

--- src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
+++ src/inventory/sns/user-tuna-tv-online/user-tuna-tv-online.service.ts
@@ -44,8 +44,8 @@ export class UserTunaTvOnlineService {
         .createQueryBuilder('tuna_tv_online')
         .select('tuna_tv_online.tuna_tv_id', 'tuna_tv_id')
         .addSelect('tuna_tv_online.online_type', 'online_type')
-        .addSelect('tuna_tv_online.user_id', 'follow_user_id')
         .addSelect('tuna_tv')
+        .addSelect('tuna_tv.user_id', 'follow_user_id')
         .addSelect('user.nickname', 'nickname')
         .addSelect('user.level', 'level')
         .innerJoin(
