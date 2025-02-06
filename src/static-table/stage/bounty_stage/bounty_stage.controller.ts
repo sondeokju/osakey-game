3809c138615c3c2855d2eeea92b7ff7a290e16b5
sondeokju <thsejrwn78@naver.com>
2025-02-06 15:42:46 +0900

    log

--- src/static-table/stage/bounty_stage/bounty_stage.controller.ts
+++ src/static-table/stage/bounty_stage/bounty_stage.controller.ts
@@ -2,6 +2,6 @@ import { Controller } from '@nestjs/common';
 import { BountyStageService } from './bounty_stage.service';
 
 @Controller('bounty/stage')
-export class BattleStageController {
+export class BountyStageController {
   constructor(private readonly bountyStageService: BountyStageService) {}
 }
