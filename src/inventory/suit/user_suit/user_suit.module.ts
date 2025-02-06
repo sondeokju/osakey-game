9b7a6f2412ff002b26d671d38bba89e72d365314
sondeokju <thsejrwn78@naver.com>
2025-02-06 13:26:52 +0900

    log

--- src/inventory/suit/user_suit/user_suit.module.ts
+++ src/inventory/suit/user_suit/user_suit.module.ts
@@ -5,11 +5,12 @@ import { UserSuitController } from './user_suit.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SuitModule } from 'src/static-table/suit/suit/suit.module';
 import { UserItem } from 'src/user_item/entities/user_item.entity';
+import { UserItemService } from 'src/user_item/user_item.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserSuit, UserItem]), SuitModule],
   exports: [UserSuitService],
   controllers: [UserSuitController],
-  providers: [UserSuitService],
+  providers: [UserSuitService, UserItemService],
 })
 export class UserSuitModule {}
