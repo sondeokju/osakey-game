1d3f652432d65ef7a5a30fdf12ee400a5c443d9a
sondeokju <thsejrwn78@naver.com>
2025-02-08 00:09:54 +0900

    log

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,21 +1,21 @@
-import { Module } from '@nestjs/common';
-import { RedisService } from './redis.service';
-import { RedisController } from './redis.controller';
-import { RedisModule as NestRedisModule } from '@liaoliaots/nestjs-redis';
+// import { Module } from '@nestjs/common';
+// import { RedisService } from './redis.service';
+// import { RedisController } from './redis.controller';
+// import { RedisModule as NestRedisModule } from '@liaoliaots/nestjs-redis';
 
-@Module({
-  imports: [
-    NestRedisModule.forRoot({
-      closeClient: true, // 앱이 종료될 때 Redis 연결도 닫음
-      readyLog: true, // Redis가 준비되면 로그 출력
-      config: {
-        host: 'localhost', // Redis 호스트
-        port: 6379, // Redis 포트
-      },
-    }),
-  ],
-  controllers: [RedisController],
-  providers: [RedisService],
-  exports: [RedisService],
-})
-export class RedisModule {}
+// @Module({
+//   imports: [
+//     NestRedisModule.forRoot({
+//       closeClient: true, // 앱이 종료될 때 Redis 연결도 닫음
+//       readyLog: true, // Redis가 준비되면 로그 출력
+//       config: {
+//         host: 'localhost', // Redis 호스트
+//         port: 6379, // Redis 포트
+//       },
+//     }),
+//   ],
+//   controllers: [RedisController],
+//   providers: [RedisService],
+//   exports: [RedisService],
+// })
+// export class RedisModule {}
