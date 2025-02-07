5251e1ef3ea2fcf8edbe5676dce0eb7a8730df5a
sondeokju <thsejrwn78@naver.com>
2025-02-08 00:05:41 +0900

    log

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { RedisController } from './redis.controller';
-//import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
 import { RedisModule as NestRedisModule } from '@liaoliaots/nestjs-redis';
 
 @Module({
   imports: [
     NestRedisModule.forRoot({
+      closeClient: true, // 앱이 종료될 때 Redis 연결도 닫음
       readyLog: true, // Redis가 준비되면 로그 출력
       config: {
         host: 'localhost', // Redis 호스트
         port: 6379, // Redis 포트
-        // password: 'bitnami', // 비밀번호 설정 (필요한 경우)
       },
     }),
   ],
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
-// import { InjectRedis } from '@nestjs-modules/ioredis';
-// import { Redis } from 'ioredis';
+import { Inject, Injectable } from '@nestjs/common';
+import { Redis } from 'ioredis';
 
 @Injectable()
 export class RedisService {
-  // constructor(@InjectRedis() private readonly redis: Redis) {}
-  // async setKey(key: string, value: string, ttl = 60): Promise<void> {
-  //   await this.redis.set(key, value, 'EX', ttl); // 60초 후 만료
-  // }
-  // async getKey(key: string): Promise<string | null> {
-  //   return await this.redis.get(key);
-  // }
-  // async deleteKey(key: string): Promise<void> {
-  //   await this.redis.del(key);
-  // }
+  constructor(@Inject('RedisClient') private readonly redis: Redis) {}
+
+  async setKey(key: string, value: string, ttl = 60): Promise<void> {
+    await this.redis.set(key, value, 'EX', ttl); // 60초 후 만료
+  }
+
+  async getKey(key: string): Promise<string | null> {
+    return await this.redis.get(key);
+  }
+
+  async deleteKey(key: string): Promise<void> {
+    await this.redis.del(key);
+  }
 }
