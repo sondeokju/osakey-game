81c15a85e2a31393d444becabad2db4a25278758
sondeokju <thsejrwn78@naver.com>
2025-02-10 21:41:31 +0900

    log

--- src/supervisor/reward_offer/reward_offer.service.ts
+++ src/supervisor/reward_offer/reward_offer.service.ts
@@ -127,6 +127,7 @@ export class RewardOfferService {
     obj['item_id'] = itemData.item_id;
     obj['item_type'] = itemData.item_type;
     obj['item_name'] = itemData.item_name;
+    //obj['item_count'] = itemData.;
 
     result.push(obj);
 
