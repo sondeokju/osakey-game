45fc2ebbdd3a349932022cb0537a5e4970580fd8
sondeokju <thsejrwn78@naver.com>
2025-02-14 13:58:45 +0900

    log

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -5,21 +5,21 @@ import { RedisService } from './redis.service';
 export class RedisController {
   constructor(private readonly redisService: RedisService) {}
 
-  // @Get('set/:key/:value')
-  // async set(@Param('key') key: string, @Param('value') value: string) {
-  //   await this.redisService.setKey(key, value);
-  //   return { message: `Key "${key}" set with value "${value}"` };
-  // }
+  @Get('set/:key/:value')
+  async set(@Param('key') key: string, @Param('value') value: string) {
+    await this.redisService.setKey(key, value);
+    return { message: `Key "${key}" set with value "${value}"` };
+  }
 
-  // @Get('get/:key')
-  // async get(@Param('key') key: string) {
-  //   const value = await this.redisService.getKey(key);
-  //   return { key, value };
-  // }
+  @Get('get/:key')
+  async get(@Param('key') key: string) {
+    const value = await this.redisService.getKey(key);
+    return { key, value };
+  }
 
-  // @Get('del/:key')
-  // async delete(@Param('key') key: string) {
-  //   await this.redisService.deleteKey(key);
-  //   return { message: `Key "${key}" deleted` };
-  // }
+  @Get('del/:key')
+  async delete(@Param('key') key: string) {
+    await this.redisService.deleteKey(key);
+    return { message: `Key "${key}" deleted` };
+  }
 }
