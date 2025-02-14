247496b5db899488ba9e6ab1b77bbe0d07cd3114
sondeokju <thsejrwn78@naver.com>
2025-02-14 14:26:09 +0900

    log

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,19 +1,20 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Redis } from 'ioredis';
 
 @Injectable()
 export class RedisService {
-  constructor(@Inject('RedisClient') private readonly redis: Redis) {}
+  constructor(@InjectRedis() private readonly redisClient: Redis) {}
 
   async setKey(key: string, value: string, ttl = 60): Promise<void> {
-    await this.redis.set(key, value, 'EX', ttl); // 60초 후 만료
+    await this.redisClient.set(key, value, 'EX', ttl); // 60초 후 만료
   }
 
   async getKey(key: string): Promise<string | null> {
-    return await this.redis.get(key);
+    return await this.redisClient.get(key);
   }
 
   async deleteKey(key: string): Promise<void> {
-    await this.redis.del(key);
+    await this.redisClient.del(key);
   }
 }
