8cb5e9bb599070238e3df4491084a9068fd11bdb
sondeokju <thsejrwn78@naver.com>
2025-02-14 13:57:40 +0900

    log

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -1,25 +1,25 @@
-// import { Controller, Get, Param } from '@nestjs/common';
-// import { RedisService } from './redis.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { RedisService } from './redis.service';
 
-// @Controller('redis')
-// export class RedisController {
-//   constructor(private readonly redisService: RedisService) {}
+@Controller('redis')
+export class RedisController {
+  constructor(private readonly redisService: RedisService) {}
 
-//   // @Get('set/:key/:value')
-//   // async set(@Param('key') key: string, @Param('value') value: string) {
-//   //   await this.redisService.setKey(key, value);
-//   //   return { message: `Key "${key}" set with value "${value}"` };
-//   // }
+  // @Get('set/:key/:value')
+  // async set(@Param('key') key: string, @Param('value') value: string) {
+  //   await this.redisService.setKey(key, value);
+  //   return { message: `Key "${key}" set with value "${value}"` };
+  // }
 
-//   // @Get('get/:key')
-//   // async get(@Param('key') key: string) {
-//   //   const value = await this.redisService.getKey(key);
-//   //   return { key, value };
-//   // }
+  // @Get('get/:key')
+  // async get(@Param('key') key: string) {
+  //   const value = await this.redisService.getKey(key);
+  //   return { key, value };
+  // }
 
-//   // @Get('del/:key')
-//   // async delete(@Param('key') key: string) {
-//   //   await this.redisService.deleteKey(key);
-//   //   return { message: `Key "${key}" deleted` };
-//   // }
-// }
+  // @Get('del/:key')
+  // async delete(@Param('key') key: string) {
+  //   await this.redisService.deleteKey(key);
+  //   return { message: `Key "${key}" deleted` };
+  // }
+}
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,19 +1,19 @@
-// import { Inject, Injectable } from '@nestjs/common';
-// import { Redis } from 'ioredis';
+import { Inject, Injectable } from '@nestjs/common';
+import { Redis } from 'ioredis';
 
-// @Injectable()
-// export class RedisService {
-//   constructor(@Inject('RedisClient') private readonly redis: Redis) {}
+@Injectable()
+export class RedisService {
+  constructor(@Inject('RedisClient') private readonly redis: Redis) {}
 
-//   async setKey(key: string, value: string, ttl = 60): Promise<void> {
-//     await this.redis.set(key, value, 'EX', ttl); // 60초 후 만료
-//   }
+  async setKey(key: string, value: string, ttl = 60): Promise<void> {
+    await this.redis.set(key, value, 'EX', ttl); // 60초 후 만료
+  }
 
-//   async getKey(key: string): Promise<string | null> {
-//     return await this.redis.get(key);
-//   }
+  async getKey(key: string): Promise<string | null> {
+    return await this.redis.get(key);
+  }
 
-//   async deleteKey(key: string): Promise<void> {
-//     await this.redis.del(key);
-//   }
-// }
+  async deleteKey(key: string): Promise<void> {
+    await this.redis.del(key);
+  }
+}
