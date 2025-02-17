9a08b6ae6df5dd0fd3483d9cd7aa810d02050a5b
sondeokju <thsejrwn78@naver.com>
2025-02-17 18:28:52 +0900

    log

--- src/inventory/achievement/user_achievements/user_achievements.service.ts
+++ src/inventory/achievement/user_achievements/user_achievements.service.ts
@@ -37,7 +37,7 @@ export class UserAchievementsService {
         'uar.season AS season',
         'uar.achieve_point AS achieve_point',
         //'RANK() OVER (ORDER BY uar.achieve_point DESC) AS rank_position',
-        'RANK() OVER (ORDER BY uar.achieve_point DESC, uar.update_at ASC) AS rank_position',
+        'ROW_NUMBER() OVER (ORDER BY uar.achieve_point DESC, uar.update_at ASC) AS rank_position',
       ])
       .from('user_achieve_ranking', 'uar')
       .limit(100);
