1580160b288782ef8d287a164616a4afa4c541ce
sondeokju <thsejrwn78@naver.com>
2025-02-19 16:37:28 +0900

    log

--- src/inventory/shop/user_shop_limit/user_shop_limit.module.ts
+++ src/inventory/shop/user_shop_limit/user_shop_limit.module.ts
@@ -4,9 +4,16 @@ import { UserShopLimitController } from './user_shop_limit.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserShopLimit } from './entities/user_shop_limit.entity';
 import { RewardOfferModule } from 'src/supervisor/reward_offer/reward_offer.module';
+import { ShopModule } from 'src/static-table/shop/shop/shop.module';
+import { ShopPackageModule } from 'src/static-table/shop/shop_package/shop_package.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserShopLimit]), RewardOfferModule],
+  imports: [
+    TypeOrmModule.forFeature([UserShopLimit]),
+    ShopModule,
+    ShopPackageModule,
+    RewardOfferModule,
+  ],
   exports: [UserShopLimitService],
   controllers: [UserShopLimitController],
   providers: [UserShopLimitService],
