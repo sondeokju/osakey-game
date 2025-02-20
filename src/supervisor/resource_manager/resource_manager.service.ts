bbb6750b9083841ce222e44da4add672e782df2c
sondeokju <thsejrwn78@naver.com>
2025-02-20 13:39:07 +0900

    log

--- src/supervisor/resource_manager/resource_manager.service.ts
+++ src/supervisor/resource_manager/resource_manager.service.ts
@@ -63,6 +63,7 @@ export class ResourceManagerService {
       // 🔹 고드(Gord) 차감
       if (resources.gord) {
         if (resources.gord < 0 || resources.gord > userCurrency.gord) {
+          console.log('고드(Gord) 차감.gord');
           errorCode = 'INSUFFICIENT_GORD';
           errorMessage = 'Not enough Gord.';
         } else {
