2e74d8f3da2323f86f24e66c9a719489492a3a0a
sondeokju <thsejrwn78@naver.com>
2025-02-25 19:13:06 +0900

    log

--- src/inventory/dispatch/user_rentama_equip_slot/user_rentama_equip_slot.module.ts
+++ src/inventory/dispatch/user_rentama_equip_slot/user_rentama_equip_slot.module.ts
@@ -3,9 +3,17 @@ import { UserRentamaEquipSlotService } from './user_rentama_equip_slot.service';
 import { UserRentamaEquipSlotController } from './user_rentama_equip_slot.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRentamaEquipSlot } from './entities/user_rentama_equip_slot.entity';
+import { UserEquipModule } from 'src/inventory/equipment/user_equip/user_equip.module';
+import { EquipGradeModule } from 'src/static-table/equipment/equip_grade/equip_grade.module';
+import { EquipLevelModule } from 'src/static-table/equipment/equip_level/equip_level.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRentamaEquipSlot])],
+  imports: [
+    TypeOrmModule.forFeature([UserRentamaEquipSlot]),
+    UserEquipModule,
+    EquipGradeModule,
+    EquipLevelModule,
+  ],
   exports: [UserRentamaEquipSlotService],
   controllers: [UserRentamaEquipSlotController],
   providers: [UserRentamaEquipSlotService],
--- src/inventory/dispatch/user_rentama_equip_slot/user_rentama_equip_slot.service.ts
+++ src/inventory/dispatch/user_rentama_equip_slot/user_rentama_equip_slot.service.ts
@@ -8,12 +8,18 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryRunner, Repository } from 'typeorm';
 import { UserRentamaEquipSlot } from './entities/user_rentama_equip_slot.entity';
+import { UserEquipService } from 'src/inventory/equipment/user_equip/user_equip.service';
+import { EquipGradeService } from 'src/static-table/equipment/equip_grade/equip_grade.service';
+import { EquipLevelService } from 'src/static-table/equipment/equip_level/equip_level.service';
 
 @Injectable()
 export class UserRentamaEquipSlotService {
   constructor(
     @InjectRepository(UserRentamaEquipSlot)
     private readonly userRentamaEquipSlotRepository: Repository<UserRentamaEquipSlot>,
+    private readonly userEquipService: UserEquipService,
+    private readonly equipGradeService: EquipGradeService,
+    private readonly equipLevelService: EquipLevelService,
   ) {}
 
   getUserRentamaEquipSlotRepository(qr?: QueryRunner) {
