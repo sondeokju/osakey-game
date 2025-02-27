e04bcd5fc5b08f575e703a9a976dab73f62f90c9
sondeokju <thsejrwn78@naver.com>
2025-02-27 13:56:16 +0900

    log

--- src/supervisor/gacha_draw/user_gacha_check/user_gacha_check.service.ts
+++ src/supervisor/gacha_draw/user_gacha_check/user_gacha_check.service.ts
@@ -102,11 +102,12 @@ export class UserGachaCheckService {
 
     return updatedUserGachaCheck;
   }
+
   async defaultGachaCountSetting(
     user_id: string,
     gacha_id: number,
-    fixed_1_count: number,
-    fixed_2_count: number,
+    fixed_1_draw_count: number,
+    fixed_2_draw_count: number,
     qr?: QueryRunner,
   ) {
     const userGachaCheckRepository = this.getUserGachaCheckRepository(qr);
@@ -115,20 +116,20 @@ export class UserGachaCheckService {
     });
 
     if (!userGachaCheck) {
-      userGachaCheck = userGachaCheckRepository.create({
+      // userGachaCheck가 없으면 새로운 데이터 삽입
+      await userGachaCheckRepository.insert({
         user_id,
         gacha_id,
-        fixed_1_draw_count: 0,
-        fixed_item_grade_1: 0,
-        fixed_item_grade_1_count: fixed_1_count,
-        fixed_2_draw_count: 0,
-        fixed_item_grade_2: 0,
-        fixed_item_grade_2_count: fixed_2_count,
+        fixed_1_draw_count,
+        fixed_2_draw_count,
+      });
+
+      // 삽입된 데이터 다시 조회
+      userGachaCheck = await userGachaCheckRepository.findOne({
+        where: { user_id, gacha_id },
       });
     }
 
-    const updatedUserGachaCheck =
-      await userGachaCheckRepository.save(userGachaCheck);
-    return updatedUserGachaCheck;
+    return userGachaCheck;
   }
 }
