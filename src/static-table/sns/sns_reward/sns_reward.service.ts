1ac58ce7fa05d0f3a20553bda26a1f4220170c23
sondeokju <thsejrwn78@naver.com>
2025-02-28 20:39:00 +0900

    log

--- src/static-table/sns/sns_reward/sns_reward.service.ts
+++ src/static-table/sns/sns_reward/sns_reward.service.ts
@@ -28,14 +28,31 @@ export class SnsRewardService {
     return result;
   }
 
+  // async getSnsReward(like_cnt: number, qr?: QueryRunner) {
+  //   const snsRewardRepository = this.getSnsRewardRepository(qr);
+  //   const result = await snsRewardRepository.findOne({
+  //     where: {
+  //       like_min: LessThanOrEqual(like_cnt), // like_cnt >= like_min
+  //       like_max: MoreThanOrEqual(like_cnt), // like_cnt <= like_max
+  //     },
+  //   });
+
+  //   return result;
+  // }
+
   async getSnsReward(like_cnt: number, qr?: QueryRunner) {
     const snsRewardRepository = this.getSnsRewardRepository(qr);
-    const result = await snsRewardRepository.findOne({
-      where: {
-        like_min: LessThanOrEqual(like_cnt), // like_cnt >= like_min
-        like_max: MoreThanOrEqual(like_cnt), // like_cnt <= like_max
-      },
-    });
+
+    const result = await snsRewardRepository
+      .createQueryBuilder('sns_reward')
+      .where('sns_reward.like_min <= :like_cnt', { like_cnt })
+      .andWhere(
+        '(sns_reward.like_max >= :like_cnt OR sns_reward.like_max = 0)',
+        { like_cnt },
+      )
+      .orderBy('sns_reward.id', 'ASC') // 정렬 필요시 추가
+      .limit(1)
+      .getOne();
 
     return result;
   }
