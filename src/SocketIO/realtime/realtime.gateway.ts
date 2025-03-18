c220d5a2d47b3ac62b5f1d4c7c47cd4549883070
sondeokju <thsejrwn78@naver.com>
2025-03-18 16:39:08 +0900

    log

--- src/SocketIO/realtime/realtime.gateway.ts
+++ src/SocketIO/realtime/realtime.gateway.ts
@@ -16,8 +16,24 @@ export class RealTimeGateway implements OnGatewayConnection {
   }
 
   // sockt.on('send_mesage', (message)=>{console.log(message)}));
+  // @SubscribeMessage('send_message')
+  // sendMessage(@MessageBody() message: string) {
+  //   console.log('send_message:', message.data);
+  // }
+
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() message: string) {
-    console.log('send_message:', message['data']);
+  sendMessage(@MessageBody() message: any) {
+    if (typeof message === 'string') {
+      try {
+        const parsedMessage = JSON.parse(message);
+        console.log('send_message:', parsedMessage.data);
+      } catch (error) {
+        console.error('Invalid JSON string:', message);
+      }
+    } else if (typeof message === 'object' && message !== null) {
+      console.log('send_message:', message.data);
+    } else {
+      console.error('Unexpected message format:', message);
+    }
   }
 }
