99a406031cb78fe7456a79f3115ed7f5e61e223e
sondeokju <thsejrwn78@naver.com>
2025-03-18 18:08:29 +0900

    log

--- src/gateways/websocket.module.ts
+++ src/gateways/websocket.module.ts
@@ -1,31 +1,8 @@
-import { Module, OnModuleInit } from '@nestjs/common';
-import { DynamicModule } from '@nestjs/common/interfaces';
-import { WebSocketGateway } from '@nestjs/websockets';
+import { Module } from '@nestjs/common';
+import { UserGateway } from './user/user.gateway';
 
-//const GATEWAYS = ['chat', 'game', 'notification', 'user', 'matchmaking'];
-const GATEWAYS = ['user'];
-
-@Module({})
-export class WebSocketModule implements OnModuleInit {
-  static register(): DynamicModule {
-    const gateways = GATEWAYS.map((namespace) => {
-      @WebSocketGateway({ namespace })
-      class DynamicGateway {
-        handleConnection(socket) {
-          console.log(`✅ ${namespace} WebSocket 연결됨 2: ${socket.id}`);
-        }
-      }
-      return DynamicGateway;
-    });
-
-    return {
-      module: WebSocketModule,
-      providers: gateways,
-      exports: gateways,
-    };
-  }
-
-  onModuleInit() {
-    console.log('✅ WebSocket 모듈이 초기화되었습니다.');
-  }
-}
+@Module({
+  providers: [UserGateway], // ✅ UserGateway는 제외!
+  exports: [UserGateway],
+})
+export class WebSocketModule {}
