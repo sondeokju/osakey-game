44bea9414211816e64599457daec97e4d04ba4ef
sondeokju <thsejrwn78@naver.com>
2025-03-19 13:59:45 +0900

    log

--- src/gateways/user/user.module.ts
+++ src/gateways/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserGateway } from './user.gateway';
 import { UserService } from './user.service';
 import { ZLoginLogModule } from 'src/game_log/login/z_login_log/z_login_log.module';
+import { ZLoginLogService } from 'src/game_log/login/z_login_log/z_login_log.service';
 
 @Module({
   imports: [ZLoginLogModule],
-  providers: [UserGateway, UserService],
+  providers: [UserGateway, UserService, ZLoginLogService],
   exports: [UserService],
 })
 export class UserModule {}
