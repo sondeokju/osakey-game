689a9b2b3a000c3dd0f1b2606e194dd688c6e555
sondeokju <thsejrwn78@naver.com>
2025-03-19 10:42:19 +0900

    log

--- src/gateways/websocket.module.ts
+++ src/gateways/websocket.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserGateway } from './user/user.gateway';
+import { UserModule } from './user/user.module';
 
 @Module({
+  imports: [UserModule],
   providers: [UserGateway], // ✅ UserGateway는 제외!
   exports: [UserGateway],
 })
