47278978bcd0b3b74e334f6ac330dd77575b8555
sondeokju <thsejrwn78@naver.com>
2025-03-19 14:03:10 +0900

    log

--- src/gateways/user/user.module.ts
+++ src/gateways/user/user.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserGateway } from './user.gateway';
 import { UserService } from './user.service';
-import { ZLoginLogModule } from 'src/game_log/login/z_login_log/z_login_log.module';
-import { ZLoginLogService } from 'src/game_log/login/z_login_log/z_login_log.service';
 
 @Module({
-  imports: [ZLoginLogModule],
-  providers: [UserGateway, UserService, ZLoginLogService],
+  imports: [],
+  providers: [UserGateway, UserService],
   exports: [UserService],
 })
 export class UserModule {}
--- src/gateways/websocket.module.ts
+++ src/gateways/websocket.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserGateway } from './user/user.gateway';
 import { UserModule } from './user/user.module';
+import { ZLoginLogModule } from 'src/game_log/login/z_login_log/z_login_log.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ZLoginLogModule],
   providers: [UserGateway], // ✅ UserGateway는 제외!
   exports: [UserGateway],
 })
