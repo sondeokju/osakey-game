f1ab319ab284202829410043df8483ee37aad03d
sondeokju <thsejrwn78@naver.com>
2025-03-19 15:14:36 +0900

    log

--- src/gateways/user/user.gateway.ts
+++ src/gateways/user/user.gateway.ts
@@ -28,7 +28,7 @@ export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
     console.log(`✅ WebSocket 1`);
 
     //const userId = socket.handshake.query.userId as string; // ✅ 쿼리스트링에서 userId 가져오기
-    const accessToken = socket.handshake.headers['accessToken'] as string;
+    const accessToken = socket.handshake.headers['accessToken'];
     console.log('accessToken:', accessToken);
 
     const userId = '';
