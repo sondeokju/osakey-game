5b67b416676c7a0a08e896c2c5d5f71625145c99
sondeokju <thsejrwn78@naver.com>
2025-03-20 15:39:32 +0900

    log

--- src/gateways/websocket.module.ts
+++ src/gateways/websocket.module.ts
@@ -3,9 +3,10 @@ import { UserGateway } from './user/user.gateway';
 import { UserModule } from './user/user.module';
 import { ZLoginLogModule } from 'src/game_log/login/z_login_log/z_login_log.module';
 import { RedisModule } from 'src/redis/redis.module';
+import { GameLogsModule } from 'src/game_log/game_logs/game_logs.module';
 
 @Module({
-  imports: [UserModule, ZLoginLogModule, RedisModule],
+  imports: [UserModule, ZLoginLogModule, RedisModule, GameLogsModule],
   providers: [UserGateway], // ✅ UserGateway는 제외!
   exports: [UserGateway],
 })
